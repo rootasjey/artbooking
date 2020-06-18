6f61e63670aa4bb54d0a0805486aa4e95f600e75
rootasjey <jeremiecorpinot@outlook.com>
2020-06-18 09:26:11 +0200

    🔧 Firebase init

---
+++ lib/generated_plugin_registrant.dart
@@ -0,0 +1,26 @@
+//
+// Generated file. Do not edit.
+//
+
+// ignore: unused_import
+import 'dart:ui';
+
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
+import 'package:cloud_functions_web/cloud_functions_web.dart';
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+// ignore: public_member_api_docs
+void registerPlugins(PluginRegistry registry) {
+  FirestoreWeb.registerWith(registry.registrarFor(FirestoreWeb));
+  CloudFunctionsWeb.registerWith(registry.registrarFor(CloudFunctionsWeb));
+  FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
+  FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
+  SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
+  UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
+  registry.registerMessageHandler();
+}
