3a205fc92d9b82d25fda0af292e452d4a44290da
rootasjey <rootasjey@users.noreply.github.com>
2021-01-01 16:38:27 +0100

    ☝️ Update dashboard as a container for inner views

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,18 +1,15 @@
-import 'package:artbooking/components/full_page_loading.dart';
 import 'package:artbooking/components/sidebar_header.dart';
-import 'package:artbooking/components/sliver_appbar_header.dart';
 import 'package:artbooking/screens/illustrations.dart';
-import 'package:artbooking/screens/signin.dart';
+import 'package:artbooking/screens/stats_overview.dart';
 import 'package:artbooking/state/colors.dart';
-import 'package:artbooking/state/user.dart';
-import 'package:artbooking/types/enums.dart';
-import 'package:artbooking/types/user/user_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:jiffy/jiffy.dart';
 
 class Dashboard extends StatefulWidget {
+  final int initialIndex;
+
+  Dashboard({this.initialIndex = 0});
+
   @override
   _DashboardState createState() => _DashboardState();
 }
@@ -21,77 +18,25 @@ class _DashboardState extends State<Dashboard> {
   bool smallViewVisible = false;
   bool isLoading = false;
 
-  double storageUsed = 0.0;
+  int _sectionIndex = 0;
 
-  ScrollController _scrollController = ScrollController();
-
-  UserFirestore userFirestore;
-  var _selectedSection = UserMenuSelect.dashboard;
+  List<Widget> _sectionsChildren = [
+    StatsOverview(),
+    Illustrations(),
+  ];
 
   @override
   void initState() {
     super.initState();
-    fetch();
+
+    setState(() {
+      _sectionIndex = widget.initialIndex;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return wideView();
-  }
-
-  Widget mainContent() {
-    if (isLoading) {
-      return Center(
-        child: FullPageLoading(),
-      );
-    }
-
-    return CustomScrollView(
-      controller: _scrollController,
-      slivers: <Widget>[
-        SliverAppHeader(),
-        SliverList(
-          delegate: SliverChildListDelegate([
-            mainContentTitle(),
-            squareStatsRow(),
-            textsStatsRow(),
-            Padding(padding: const EdgeInsets.only(bottom: 200.0)),
-          ]),
-        ),
-      ],
-    );
-  }
-
-  Widget mainContentTitle() {
-    return Padding(
-      padding: const EdgeInsets.only(
-        top: 20.0,
-        left: 60.0,
-        bottom: 40.0,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Opacity(
-            opacity: 0.6,
-            child: Text(
-              'Dashboard',
-              style: TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w200,
-              ),
-            ),
-          ),
-          Text(
-            'Activity',
-            style: TextStyle(
-              fontSize: 90.0,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-        ],
-      ),
-    );
   }
 
   Widget leftSide() {
@@ -100,7 +45,7 @@ class _DashboardState extends State<Dashboard> {
         if (!smallViewVisible)
           Padding(
             padding: const EdgeInsets.only(
-              top: 40.0,
+              top: 30.0,
               bottom: 100.0,
               left: 20.0,
               right: 20.0,
@@ -108,44 +53,52 @@ class _DashboardState extends State<Dashboard> {
             child: SideBarHeader(),
           ),
         sectionTileItem(
-          title: "Illustrations",
+          index: 0,
+          title: 'Activity',
+          leading: Icon(Icons.show_chart_rounded),
+        ),
+        sectionTileItem(
+          index: 1,
+          title: 'Illustrations',
           leading: Icon(Icons.image),
-          section: UserMenuSelect.illustrations,
         ),
         sectionTileItem(
-          title: "Books",
+          index: 2,
+          title: 'Books',
           leading: Icon(Icons.my_library_books),
-          section: UserMenuSelect.books,
         ),
         sectionTileItem(
-          title: "Galleries",
+          index: 3,
+          title: 'Galleries',
           // leading: Icon(Icons.home_work_rounded),
           leading: Icon(FontAwesomeIcons.landmark),
-          section: UserMenuSelect.galleries,
         ),
         sectionTileItem(
-          title: "Challenges",
+          index: 4,
+          title: 'Challenges',
           leading: Icon(FontAwesomeIcons.award),
-          section: UserMenuSelect.challenges,
         ),
         sectionTileItem(
-          title: "Contests",
+          index: 5,
+          title: 'Contests',
           leading: Icon(FontAwesomeIcons.trophy),
-          section: UserMenuSelect.contests,
         ),
-        // Padding(
-        //   padding: const EdgeInsets.only(bottom: 20.0),
-        // ),
-        Divider(thickness: 1.0, color: stateColors.foreground, height: 40.0),
+        Divider(
+          thickness: 0.5,
+          color: stateColors.foreground,
+          height: 40.0,
+          indent: 60.0,
+          endIndent: 60.0,
+        ),
         sectionTileItem(
-          title: "Settings",
+          index: 6,
+          title: 'Settings',
           leading: Icon(Icons.settings),
-          section: UserMenuSelect.settings,
         ),
         sectionTileItem(
-          title: "About",
+          index: 7,
+          title: 'About',
           leading: Icon(Icons.help),
-          section: UserMenuSelect.about,
         ),
         Padding(padding: const EdgeInsets.only(bottom: 60.0)),
       ],
@@ -155,9 +108,10 @@ class _DashboardState extends State<Dashboard> {
   Widget sectionTileItem({
     @required String title,
     Widget leading,
-    @required UserMenuSelect section,
+    @required index,
   }) {
     return ListTile(
+      selected: _sectionIndex == index,
       contentPadding: const EdgeInsets.symmetric(
         horizontal: 24.0,
         vertical: 4.0,
@@ -170,10 +124,10 @@ class _DashboardState extends State<Dashboard> {
       ),
       leading: leading,
       trailing:
-          section == _selectedSection ? Icon(Icons.keyboard_arrow_right) : null,
+          _sectionIndex == index ? Icon(Icons.keyboard_arrow_right) : null,
       onTap: () {
         setState(() {
-          _selectedSection = section;
+          _sectionIndex = index;
         });
 
         // final width = MediaQuery.of(context).size.width;
@@ -193,220 +147,15 @@ class _DashboardState extends State<Dashboard> {
               foregroundDecoration: BoxDecoration(
                 color: Color.fromRGBO(0, 0, 0, 0.05),
               ),
-              // padding: const EdgeInsets.symmetric(
-              //   horizontal: 20.0,
-              // ),
               child: leftSide(),
             ),
           ),
           Expanded(
             flex: 3,
-            child: mainContent(),
+            child: _sectionsChildren[_sectionIndex],
           ),
         ],
       ),
     );
-  }
-
-  Widget squareStatsRow() {
-    final stats = stateUser.userFirestore.stats;
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 50.0,
-        vertical: 60.0,
-      ),
-      child: Wrap(
-        spacing: 10.0,
-        runSpacing: 10.0,
-        alignment: WrapAlignment.start,
-        children: <Widget>[
-          squareStats(
-            title: "Illustrations",
-            count: stats.images.own,
-            onTap: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(builder: (_) => Illustrations()),
-              );
-            },
-          ),
-          squareStats(
-            title: "Books",
-            count: stats.books.own,
-            onTap: () {},
-          ),
-          squareStats(
-            title: "Galleries",
-            count: stats.galleries.own,
-            onTap: () {},
-          ),
-          squareStats(
-            title: "Challenges",
-            count: stats.challenges.participating,
-            onTap: () {},
-          ),
-          squareStats(
-            title: "Contests",
-            count: stats.constests.participating,
-            onTap: () {},
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget squareStats({
-    @required String title,
-    @required int count,
-    @required VoidCallback onTap,
-  }) {
-    return SizedBox(
-      width: 200.0,
-      height: 200.0,
-      child: Card(
-        elevation: 4.0,
-        child: InkWell(
-          onTap: onTap,
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  count.toString(),
-                  style: TextStyle(
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.w200,
-                  ),
-                ),
-                Text(
-                  title,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget textsStatsRow() {
-    return Padding(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 50.0,
-        vertical: 60.0,
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          FlatButton.icon(
-            onPressed: () {},
-            icon: Icon(Icons.storage),
-            label: RichText(
-              text: TextSpan(
-                style: TextStyle(
-                  color: stateColors.foreground,
-                  fontSize: 20.0,
-                ),
-                children: [
-                  TextSpan(text: "Your total used space is "),
-                  TextSpan(
-                    text: "${getUsedSpace()}",
-                    style: TextStyle(color: stateColors.accent),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          FlatButton.icon(
-            onPressed: () {},
-            icon: Icon(Icons.timelapse),
-            label: RichText(
-              text: TextSpan(
-                style: TextStyle(
-                  color: stateColors.foreground,
-                  fontSize: 20.0,
-                ),
-                children: [
-                  TextSpan(text: "You're a member since "),
-                  TextSpan(
-                    text:
-                        '${Jiffy(stateUser.userFirestore.createdAt).format('MMMM yyyy')}',
-                    style: TextStyle(color: stateColors.accent),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-
-  void fetch() async {
-    setState(() {
-      isLoading = true;
-    });
-
-    try {
-      final userAuth = FirebaseAuth.instance.currentUser;
-
-      if (userAuth == null) {
-        WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (_) => Signin()),
-          );
-        });
-
-        return;
-      }
-
-      setState(() {
-        isLoading = false;
-      });
-    } catch (err) {
-      debugPrint(err.toString());
-
-      setState(() {
-        isLoading = false;
-      });
-    }
-  }
-
-  String getUsedSpace() {
-    final usedBytes = stateUser.userFirestore.stats.storage.images.used;
-    var units = 'bytes';
-
-    if (usedBytes < 1000) {
-      return '$usedBytes $units';
-    }
-
-    if (usedBytes < 1000000) {
-      units = 'KB';
-      return '$usedBytes $units';
-    }
-
-    if (usedBytes < 1000000000) {
-      units = 'MB';
-      return '$usedBytes $units';
-    }
-
-    if (usedBytes < 1000000000000) {
-      units = 'GB';
-      return '$usedBytes $units';
-    }
-
-    if (usedBytes < 1000000000000000) {
-      units = 'TB';
-      return '$usedBytes $units';
-    }
-
-    units = 'PB';
-    return '$usedBytes $units';
   }
 }
---
+++ lib/screens/stats_overview.dart
@@ -0,0 +1,299 @@
+import 'package:artbooking/components/full_page_loading.dart';
+import 'package:artbooking/components/sliver_appbar_header.dart';
+import 'package:artbooking/screens/dashboard.dart';
+import 'package:artbooking/screens/signin.dart';
+import 'package:artbooking/state/colors.dart';
+import 'package:artbooking/state/user.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
+
+class StatsOverview extends StatefulWidget {
+  @override
+  _StatsOverviewState createState() => _StatsOverviewState();
+}
+
+class _StatsOverviewState extends State<StatsOverview> {
+  final _scrollController = ScrollController();
+
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    fetch();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: <Widget>[
+          SliverAppHeader(),
+          SliverList(
+            delegate: SliverChildListDelegate([
+              mainContentTitle(),
+              body(),
+              Padding(padding: const EdgeInsets.only(bottom: 200.0)),
+            ]),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget body() {
+    if (isLoading) {
+      return Center(
+        child: FullPageLoading(),
+      );
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        squareStatsRow(),
+        textsStatsRow(),
+      ],
+    );
+  }
+
+  Widget mainContentTitle() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 20.0,
+        left: 60.0,
+        bottom: 40.0,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Opacity(
+            opacity: 0.6,
+            child: Text(
+              'Dashboard',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+          ),
+          Text(
+            'Activity',
+            style: TextStyle(
+              fontSize: 90.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget squareStatsRow() {
+    final stats = stateUser.userFirestore.stats;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 50.0,
+        vertical: 60.0,
+      ),
+      child: Wrap(
+        spacing: 10.0,
+        runSpacing: 10.0,
+        alignment: WrapAlignment.start,
+        children: <Widget>[
+          squareStats(
+            title: 'Illustrations',
+            count: stats.images.own,
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => Dashboard(
+                    initialIndex: 1,
+                  ),
+                ),
+              );
+            },
+          ),
+          squareStats(
+            title: 'Books',
+            count: stats.books.own,
+            onTap: () {},
+          ),
+          squareStats(
+            title: 'Galleries',
+            count: stats.galleries.own,
+            onTap: () {},
+          ),
+          squareStats(
+            title: 'Challenges',
+            count: stats.challenges.participating,
+            onTap: () {},
+          ),
+          squareStats(
+            title: 'Contests',
+            count: stats.constests.participating,
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget squareStats({
+    @required String title,
+    @required int count,
+    @required VoidCallback onTap,
+  }) {
+    return SizedBox(
+      width: 200.0,
+      height: 200.0,
+      child: Card(
+        elevation: 4.0,
+        child: InkWell(
+          onTap: onTap,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  count.toString(),
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget textsStatsRow() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 50.0,
+        vertical: 60.0,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          FlatButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.storage),
+            label: RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  color: stateColors.foreground.withOpacity(0.6),
+                  fontSize: 20.0,
+                ),
+                children: [
+                  TextSpan(text: "Your total used space is "),
+                  TextSpan(
+                    text: "${getUsedSpace()}",
+                    style: TextStyle(color: stateColors.accent),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          FlatButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.timelapse),
+            label: RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  color: stateColors.foreground.withOpacity(0.6),
+                  fontSize: 20.0,
+                ),
+                children: [
+                  TextSpan(text: "You're a member since "),
+                  TextSpan(
+                    text:
+                        '${Jiffy(stateUser.userFirestore.createdAt).format('MMMM yyyy')}',
+                    style: TextStyle(color: stateColors.accent),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void fetch() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      final userAuth = FirebaseAuth.instance.currentUser;
+
+      if (userAuth == null) {
+        WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (_) => Signin()),
+          );
+        });
+
+        return;
+      }
+
+      setState(() {
+        isLoading = false;
+      });
+    } catch (err) {
+      debugPrint(err.toString());
+
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  String getUsedSpace() {
+    final usedBytes = stateUser.userFirestore.stats.storage.images.used;
+    var units = 'bytes';
+
+    if (usedBytes < 1000) {
+      return '$usedBytes $units';
+    }
+
+    if (usedBytes < 1000000) {
+      units = 'KB';
+      return '$usedBytes $units';
+    }
+
+    if (usedBytes < 1000000000) {
+      units = 'MB';
+      return '$usedBytes $units';
+    }
+
+    if (usedBytes < 1000000000000) {
+      units = 'GB';
+      return '$usedBytes $units';
+    }
+
+    if (usedBytes < 1000000000000000) {
+      units = 'TB';
+      return '$usedBytes $units';
+    }
+
+    units = 'PB';
+    return '$usedBytes $units';
+  }
+}
