3c16c7d5696ed11ef8c1a228bea34c5005f819f6
rootasjey <rootasjey@users.noreply.github.com>
2021-01-01 04:59:29 +0100

    ☝️ Update dashboard w/ real data

---
+++ lib/components/sidebar_header.dart
@@ -0,0 +1,191 @@
+import 'package:artbooking/actions/users.dart';
+import 'package:artbooking/router/route_names.dart';
+import 'package:artbooking/screens/signin.dart';
+import 'package:artbooking/state/colors.dart';
+import 'package:artbooking/state/user.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+class SideBarHeader extends StatefulWidget {
+  @override
+  _SideBarHeaderState createState() => _SideBarHeaderState();
+}
+
+class _SideBarHeaderState extends State<SideBarHeader> {
+  @override
+  Widget build(BuildContext context) {
+    return Observer(builder: (context) {
+      return stateUser.isUserConnected ? authenticatedView() : guestView();
+    });
+  }
+
+  Widget authenticatedView() {
+    return ListTile(
+      leading: Observer(
+        builder: (context) {
+          if (stateUser.avatarUrl.isEmpty) {
+            final arrStr = stateUser.username.split(' ');
+            String initials = '';
+
+            if (arrStr.length > 0) {
+              initials = arrStr.length > 1
+                  ? arrStr
+                      .reduce((value, element) => value + element.substring(1))
+                  : arrStr.first;
+
+              if (initials != null && initials.isNotEmpty) {
+                initials = initials.substring(0, 1);
+              }
+            }
+
+            return CircleAvatar(
+              backgroundColor: stateColors.primary,
+              radius: 20.0,
+              child: Text(
+                initials,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            );
+          }
+
+          return Material(
+            elevation: 4.0,
+            shape: CircleBorder(),
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Image.asset(
+                  stateUser.avatarUrl,
+                  width: 50.0,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+      title: Tooltip(
+        message: stateUser.username,
+        child: Text(
+          stateUser.username,
+          overflow: TextOverflow.ellipsis,
+        ),
+      ),
+      trailing: PopupMenuButton<String>(
+        icon: Icon(Icons.keyboard_arrow_down),
+        tooltip: 'Menu',
+        onSelected: (value) {
+          if (value == 'signout') {
+            userSignOut(context: context);
+            return;
+          }
+
+          // Rerouter.push(
+          //   context: context,
+          //   value: value,
+          // );
+        },
+        itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+          const PopupMenuItem(
+              value: RootRoute,
+              child: ListTile(
+                leading: Icon(Icons.home),
+                title: Text(
+                  'Home',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              )),
+          const PopupMenuItem(
+            value: AccountRoute,
+            child: ListTile(
+              leading: Icon(Icons.settings),
+              title: Text(
+                'Settings',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          const PopupMenuItem(
+            value: 'signout',
+            child: ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text(
+                'Sign out',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget guestView() {
+    return Row(
+      children: <Widget>[
+        RaisedButton(
+          onPressed: () {
+            Navigator.of(context)
+                .push(MaterialPageRoute(builder: (_) => Signin()));
+          },
+          color: Colors.black12,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(7.0),
+            ),
+          ),
+          child: Container(
+            width: 100.0,
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'SIGN IN',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Spacer(),
+        PopupMenuButton<String>(
+          icon: Icon(Icons.keyboard_arrow_down),
+          tooltip: 'Menu',
+          onSelected: (value) {
+            // Rerouter.push(
+            //   context: context,
+            //   value: value,
+            // );
+          },
+          itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+            const PopupMenuItem(
+                value: RootRoute,
+                child: ListTile(
+                  leading: Icon(Icons.home),
+                  title: Text(
+                    'Home',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                )),
+            const PopupMenuItem(
+              value: AccountRoute,
+              child: ListTile(
+                leading: Icon(Icons.settings),
+                title: Text(
+                  'Settings',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,7 +1,16 @@
+import 'package:artbooking/components/full_page_loading.dart';
+import 'package:artbooking/components/sidebar_header.dart';
 import 'package:artbooking/components/sliver_appbar_header.dart';
 import 'package:artbooking/screens/illustrations.dart';
+import 'package:artbooking/screens/signin.dart';
 import 'package:artbooking/state/colors.dart';
+import 'package:artbooking/state/user.dart';
+import 'package:artbooking/types/enums.dart';
+import 'package:artbooking/types/user/user_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:jiffy/jiffy.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -9,151 +18,395 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
+  bool smallViewVisible = false;
+  bool isLoading = false;
+
+  double storageUsed = 0.0;
+
   ScrollController _scrollController = ScrollController();
+
+  UserFirestore userFirestore;
+  var _selectedSection = UserMenuSelect.dashboard;
+
+  @override
+  void initState() {
+    super.initState();
+    fetch();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(
-        controller: _scrollController,
-        slivers: <Widget>[
-          SliverAppHeader(),
-          bodyListContent(),
-        ],
-      ),
-    );
+    return wideView();
   }
 
-  Widget bodyListContent() {
-    return SliverList(
-      delegate: SliverChildListDelegate([
-        Padding(
-          padding: const EdgeInsets.only(
-            top: 80.0,
-          ),
-        ),
-        userIdentity(),
-        Divider(
-          thickness: 1.0,
-          height: 200.0,
-        ),
-        sectionsView(),
-        Padding(
-            padding: const EdgeInsets.only(
-          bottom: 200.0,
-        ))
-      ]),
-    );
-  }
+  Widget mainContent() {
+    if (isLoading) {
+      return Center(
+        child: FullPageLoading(),
+      );
+    }
 
-  Widget userIdentity() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Material(
-          elevation: 3.0,
-          shape: CircleBorder(),
-          clipBehavior: Clip.hardEdge,
-          color: Colors.transparent,
-          child: Ink.image(
-            image: NetworkImage(
-                'https://drawinghowtos.com/wp-content/uploads/2019/04/fox-colored.png'),
-            fit: BoxFit.cover,
-            width: 150.0,
-            height: 150.0,
-            child: InkWell(
-              onTap: () {
-                showDialog(
-                    context: context,
-                    barrierDismissible: true,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        content: Container(
-                          child: Image(
-                            image: AssetImage(
-                                'https://drawinghowtos.com/wp-content/uploads/2019/04/fox-colored.png'),
-                          ),
-                        ),
-                      );
-                    });
-              },
-            ),
-          ),
-        ),
-        Padding(padding: const EdgeInsets.only(left: 50.0)),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'Username',
-              style: TextStyle(
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Opacity(
-              opacity: .6,
-              child: Text(
-                'Paris, FR',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 25.0),
-              child: FlatButton(
-                onPressed: () {},
-                shape: RoundedRectangleBorder(
-                  side: BorderSide(
-                    color: stateColors.foreground,
-                  ),
-                  borderRadius: BorderRadius.all(Radius.circular(7.0)),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Text(
-                    'Edit Profile',
-                  ),
-                ),
-              ),
-            )
-          ],
+    return CustomScrollView(
+      controller: _scrollController,
+      slivers: <Widget>[
+        SliverAppHeader(),
+        SliverList(
+          delegate: SliverChildListDelegate([
+            mainContentTitle(),
+            squareStatsRow(),
+            textsStatsRow(),
+            Padding(padding: const EdgeInsets.only(bottom: 200.0)),
+          ]),
         ),
       ],
     );
   }
 
-  Widget sectionsView() {
+  Widget mainContentTitle() {
     return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Wrap(
-        alignment: WrapAlignment.center,
+      padding: const EdgeInsets.only(
+        top: 20.0,
+        left: 60.0,
+        bottom: 40.0,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Opacity(
+            opacity: 0.6,
+            child: Text(
+              'Dashboard',
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w200,
+              ),
+            ),
+          ),
+          Text(
+            'Activity',
+            style: TextStyle(
+              fontSize: 90.0,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget leftSide() {
+    return ListView(
+      children: <Widget>[
+        if (!smallViewVisible)
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 40.0,
+              bottom: 100.0,
+              left: 20.0,
+              right: 20.0,
+            ),
+            child: SideBarHeader(),
+          ),
+        sectionTileItem(
+          title: "Illustrations",
+          leading: Icon(Icons.image),
+          section: UserMenuSelect.illustrations,
+        ),
+        sectionTileItem(
+          title: "Books",
+          leading: Icon(Icons.my_library_books),
+          section: UserMenuSelect.books,
+        ),
+        sectionTileItem(
+          title: "Galleries",
+          // leading: Icon(Icons.home_work_rounded),
+          leading: Icon(FontAwesomeIcons.landmark),
+          section: UserMenuSelect.galleries,
+        ),
+        sectionTileItem(
+          title: "Challenges",
+          leading: Icon(FontAwesomeIcons.award),
+          section: UserMenuSelect.challenges,
+        ),
+        sectionTileItem(
+          title: "Contests",
+          leading: Icon(FontAwesomeIcons.trophy),
+          section: UserMenuSelect.contests,
+        ),
+        // Padding(
+        //   padding: const EdgeInsets.only(bottom: 20.0),
+        // ),
+        Divider(thickness: 1.0, color: stateColors.foreground, height: 40.0),
+        sectionTileItem(
+          title: "Settings",
+          leading: Icon(Icons.settings),
+          section: UserMenuSelect.settings,
+        ),
+        sectionTileItem(
+          title: "About",
+          leading: Icon(Icons.help),
+          section: UserMenuSelect.about,
+        ),
+        Padding(padding: const EdgeInsets.only(bottom: 60.0)),
+      ],
+    );
+  }
+
+  Widget sectionTileItem({
+    @required String title,
+    Widget leading,
+    @required UserMenuSelect section,
+  }) {
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(
+        horizontal: 24.0,
+        vertical: 4.0,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+      leading: leading,
+      trailing:
+          section == _selectedSection ? Icon(Icons.keyboard_arrow_right) : null,
+      onTap: () {
+        setState(() {
+          _selectedSection = section;
+        });
+
+        // final width = MediaQuery.of(context).size.width;
+        // if (width < 500.0) {
+        //   _innerDrawerKey.currentState.close();
+        // }
+      },
+    );
+  }
+
+  Widget wideView() {
+    return Material(
+      child: Row(
         children: <Widget>[
-          SizedBox(
-            width: 240.0,
-            height: 300.0,
-            child: Card(
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(builder: (_) => Illustrations()),
-                  );
-                },
-                child: Center(
-                  child: Text(
-                    'Illustrations',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                    ),
+          Expanded(
+            child: Container(
+              foregroundDecoration: BoxDecoration(
+                color: Color.fromRGBO(0, 0, 0, 0.05),
+              ),
+              // padding: const EdgeInsets.symmetric(
+              //   horizontal: 20.0,
+              // ),
+              child: leftSide(),
+            ),
+          ),
+          Expanded(
+            flex: 3,
+            child: mainContent(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget squareStatsRow() {
+    final stats = stateUser.userFirestore.stats;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 50.0,
+        vertical: 60.0,
+      ),
+      child: Wrap(
+        spacing: 10.0,
+        runSpacing: 10.0,
+        alignment: WrapAlignment.start,
+        children: <Widget>[
+          squareStats(
+            title: "Illustrations",
+            count: stats.images.own,
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(builder: (_) => Illustrations()),
+              );
+            },
+          ),
+          squareStats(
+            title: "Books",
+            count: stats.books.own,
+            onTap: () {},
+          ),
+          squareStats(
+            title: "Galleries",
+            count: stats.galleries.own,
+            onTap: () {},
+          ),
+          squareStats(
+            title: "Challenges",
+            count: stats.challenges.participating,
+            onTap: () {},
+          ),
+          squareStats(
+            title: "Contests",
+            count: stats.constests.participating,
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget squareStats({
+    @required String title,
+    @required int count,
+    @required VoidCallback onTap,
+  }) {
+    return SizedBox(
+      width: 200.0,
+      height: 200.0,
+      child: Card(
+        elevation: 4.0,
+        child: InkWell(
+          onTap: onTap,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  count.toString(),
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.w200,
                   ),
                 ),
+                Text(
+                  title,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget textsStatsRow() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 50.0,
+        vertical: 60.0,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          FlatButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.storage),
+            label: RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  color: stateColors.foreground,
+                  fontSize: 20.0,
+                ),
+                children: [
+                  TextSpan(text: "Your total used space is "),
+                  TextSpan(
+                    text: "${getUsedSpace()}",
+                    style: TextStyle(color: stateColors.accent),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          FlatButton.icon(
+            onPressed: () {},
+            icon: Icon(Icons.timelapse),
+            label: RichText(
+              text: TextSpan(
+                style: TextStyle(
+                  color: stateColors.foreground,
+                  fontSize: 20.0,
+                ),
+                children: [
+                  TextSpan(text: "You're a member since "),
+                  TextSpan(
+                    text:
+                        '${Jiffy(stateUser.userFirestore.createdAt).format('MMMM yyyy')}',
+                    style: TextStyle(color: stateColors.accent),
+                  ),
+                ],
               ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  void fetch() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      final userAuth = FirebaseAuth.instance.currentUser;
+
+      if (userAuth == null) {
+        WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (_) => Signin()),
+          );
+        });
+
+        return;
+      }
+
+      setState(() {
+        isLoading = false;
+      });
+    } catch (err) {
+      debugPrint(err.toString());
+
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  String getUsedSpace() {
+    final usedBytes = stateUser.userFirestore.stats.storage.images.used;
+    var units = 'bytes';
+
+    if (usedBytes < 1000) {
+      return '$usedBytes $units';
+    }
+
+    if (usedBytes < 1000000) {
+      units = 'KB';
+      return '$usedBytes $units';
+    }
+
+    if (usedBytes < 1000000000) {
+      units = 'MB';
+      return '$usedBytes $units';
+    }
+
+    if (usedBytes < 1000000000000) {
+      units = 'GB';
+      return '$usedBytes $units';
+    }
+
+    if (usedBytes < 1000000000000000) {
+      units = 'TB';
+      return '$usedBytes $units';
+    }
+
+    units = 'PB';
+    return '$usedBytes $units';
   }
 }
