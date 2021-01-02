367eb0d7321e1b97045f4b58bfe648d6617f578d
rootasjey <rootasjey@users.noreply.github.com>
2021-01-02 03:54:13 +0100

    ➕ Add AnimatedAppIcon (w/o asset)

---
+++ lib/components/animated_app_icon.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AnimatedAppIcon extends StatelessWidget {
+  final double size;
+
+  AnimatedAppIcon({this.size = 100.0});
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/images/app-icon-96.png',
+      height: size,
+      width: size,
+    );
+  }
+}
