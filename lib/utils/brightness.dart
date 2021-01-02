24aeb86ba3e90c6198a5cbcad5319101083322ad
rootasjey <rootasjey@users.noreply.github.com>
2021-01-02 03:54:40 +0100

    ➕ Add Brightness utils

---
+++ lib/utils/brightness.dart
@@ -0,0 +1,27 @@
+import 'package:artbooking/state/colors.dart';
+import 'package:artbooking/utils/app_storage.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:flutter/material.dart';
+
+/// Refresh current theme with auto brightness.
+void setAutoBrightness(BuildContext context) {
+  final now = DateTime.now();
+
+  Brightness brightness = Brightness.light;
+
+  if (now.hour < 6 || now.hour > 17) {
+    brightness = Brightness.dark;
+  }
+
+  DynamicTheme.of(context).setBrightness(brightness);
+  stateColors.refreshTheme(brightness);
+  appStorage.setAutoBrightness(true);
+}
+
+/// Refresh current theme with a specific brightness.
+void setBrightness(BuildContext context, Brightness brightness) {
+  DynamicTheme.of(context).setBrightness(brightness);
+  stateColors.refreshTheme(brightness);
+  appStorage.setAutoBrightness(false);
+  appStorage.setBrightness(brightness);
+}
