0cf11e68dc12e0405440a4fb8c00756f4a7a1ba9
rootasjey <rootasjey@users.noreply.github.com>
2021-03-05 13:12:04 +0100

    ➕ (func) Add stats f() to [index.ts]

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,11 @@
 import * as booksFunc from './books';
 import * as illustrationsFunc from './illustrations';
 import * as searchFunc from './search';
+import * as statsFunc from './stats';
 import * as usersFunc from './users';
 
 export const books = booksFunc;
 export const illustrations = illustrationsFunc;
 export const search = searchFunc;
+export const stats = statsFunc;
 export const users = usersFunc;
