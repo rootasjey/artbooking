ed636b186e7ee8e7f567ad9984e8d7fbdfe7e282
rootasjey <rootasjey@users.noreply.github.com>
2021-03-13 00:55:22 +0100

    💉 Update all dependencies

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const appVersion = '0.20.0';
+  static const appVersion = '0.40.0';
   static const appBuildNumber = 1;
   static const cardElevation = 2.0;
   static const maxMobileHeight = 400.0;
