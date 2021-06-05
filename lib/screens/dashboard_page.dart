b03c892977b4f79d950a5153fea6b5d8a050fedb
rootasjey <rootasjey@users.noreply.github.com>
2021-06-06 00:25:15 +0200

    feat(page): :sparkles: update dashboard page w/ updated router

--- lib/screens/dashboard_page.dart
+++ lib/screens/dashboard_page.dart
@@ -1,11 +1,12 @@
-import 'package:artbooking/components/main_app_bar.dart';
 import 'package:artbooking/components/side_menu_item.dart';
+import 'package:artbooking/components/underlined_button.dart';
 import 'package:artbooking/state/upload_manager.dart';
 import 'package:artbooking/router/app_router.gr.dart';
 import 'package:artbooking/state/colors.dart';
 import 'package:artbooking/utils/constants.dart';
 import 'package:artbooking/utils/fonts.dart';
 import 'package:auto_route/auto_route.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:unicons/unicons.dart';
 
@@ -17,19 +18,19 @@ class DashboardPage extends StatefulWidget {
 class _DashboardPageState extends State<DashboardPage> {
   final _sideMenuItems = <SideMenuItem>[
     SideMenuItem(
-      destination: MyActivityPageRoute(),
+      index: 0,
       iconData: UniconsLine.chart_pie,
       label: 'Activity',
       hoverColor: Colors.red,
     ),
     SideMenuItem(
-      destination: MyIllustrationsPageRoute(),
+      index: 1,
       iconData: UniconsLine.picture,
       label: 'Illustrations',
       hoverColor: Colors.red,
     ),
     SideMenuItem(
-      destination: MyBooksPageRoute(),
+      index: 2,
       iconData: UniconsLine.book_alt,
       label: 'Books',
       hoverColor: Colors.blue.shade700,
@@ -53,7 +54,7 @@ class _DashboardPageState extends State<DashboardPage> {
     //   hoverColor: Colors.yellow.shade800,
     // ),
     SideMenuItem(
-      destination: DashSettingsRouter(),
+      index: 3,
       iconData: UniconsLine.setting,
       label: 'Settings',
       hoverColor: Colors.blueGrey,
@@ -68,13 +69,24 @@ class _DashboardPageState extends State<DashboardPage> {
 
   @override
   Widget build(context) {
-    return AutoRouter(
-      builder: (context, child) {
+    return AutoTabsScaffold(
+      routes: const [
+        MyActivityPageRoute(),
+        DashIllustrationsRouter(),
+        DashBooksRouter(),
+        DashSettingsRouter(),
+      ],
+      builder: (context, child, animation) {
         return Material(
           child: Row(
             children: [
-              buildSideMenu(context),
-              Expanded(child: child),
+              buildSidePanel(context, context.tabsRouter),
+              Expanded(
+                child: Material(
+                  elevation: 6.0,
+                  child: child,
+                ),
+              ),
             ],
           ),
         );
@@ -82,112 +94,21 @@ class _DashboardPageState extends State<DashboardPage> {
     );
   }
 
-  Widget buildSideMenu(BuildContext context) {
-    final router = context.router;
-
+  Widget buildSidePanel(BuildContext context, TabsRouter tabsRouter) {
     if (MediaQuery.of(context).size.width < Constants.maxMobileWidth) {
       return Container();
     }
 
     return Container(
-      foregroundDecoration: BoxDecoration(
-        color: Color.fromRGBO(0, 0, 0, 0.05),
-      ),
+      color: stateColors.lightBackground,
       width: 300.0,
       child: Stack(
         children: <Widget>[
           CustomScrollView(
             slivers: <Widget>[
-              SliverPadding(
-                padding: const EdgeInsets.only(
-                  top: 20.0,
-                  bottom: 50.0,
-                ),
-                sliver: MainAppBar(),
-              ),
-              SliverPadding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20.0,
-                ),
-                sliver: SliverList(
-                    delegate: SliverChildListDelegate.fixed(
-                  _sideMenuItems.map((item) {
-                    Color color = stateColors.foreground.withOpacity(0.6);
-                    Color textColor = stateColors.foreground.withOpacity(0.6);
-                    FontWeight fontWeight = FontWeight.w400;
-
-                    if (item.destination.fullPath ==
-                        router.current?.route?.path) {
-                      color = item.hoverColor;
-                      textColor = stateColors.foreground;
-                      fontWeight = FontWeight.w600;
-                    }
-
-                    return ListTile(
-                      leading: Icon(
-                        item.iconData,
-                        color: color,
-                      ),
-                      title: Text(
-                        item.label,
-                        style: FontsUtils.mainStyle(
-                          color: textColor,
-                          fontWeight: fontWeight,
-                        ),
-                      ),
-                      onTap: () {
-                        if (item.destination.routeName == 'AdminDeepRoute') {
-                          item.destination.show(context);
-                          return;
-                        }
-
-                        router.navigate(item.destination);
-                      },
-                    );
-                  }).toList(),
-                )),
-              ),
+              topSidePanel(),
+              bodySidePanel(tabsRouter),
             ],
-          ),
-          Positioned(
-            left: 40.0,
-            bottom: 20.0,
-            child: ElevatedButton(
-              onPressed: () {
-                appUploadManager.pickImage(context);
-              },
-              style: ElevatedButton.styleFrom(
-                primary: stateColors.primary,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(30.0),
-                  ),
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 12.0,
-                ),
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    minWidth: 160.0,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(UniconsLine.upload, color: Colors.white),
-                      Padding(padding: const EdgeInsets.only(left: 10.0)),
-                      Text(
-                        'Upload',
-                        style: TextStyle(
-                          color: Colors.white,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
           ),
         ],
       ),
@@ -221,5 +142,119 @@ class _DashboardPageState extends State<DashboardPage> {
     //     hoverColor: Colors.red,
     //   ),
     // ]);
+  }
+
+  Widget bodySidePanel(TabsRouter tabsRouter) {
+    return SliverPadding(
+      padding: const EdgeInsets.only(
+        left: 20.0,
+        right: 20.0,
+      ),
+      sliver: SliverList(
+          delegate: SliverChildListDelegate.fixed(
+        _sideMenuItems.map((item) {
+          Color color = stateColors.foreground.withOpacity(0.6);
+          Color textColor = stateColors.foreground.withOpacity(0.4);
+          FontWeight fontWeight = FontWeight.w600;
+
+          if (tabsRouter.activeIndex == item.index) {
+            color = item.hoverColor;
+            textColor = stateColors.foreground.withOpacity(0.6);
+            fontWeight = FontWeight.w700;
+          }
+
+          return Padding(
+            padding: const EdgeInsets.only(
+              left: 24.0,
+              top: 32.0,
+            ),
+            child: UnderlinedButton(
+              leading: Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: Icon(
+                  item.iconData,
+                  color: color,
+                ),
+              ),
+              child: Text(
+                item.label,
+                style: FontsUtils.mainStyle(
+                  color: textColor,
+                  fontSize: 16.0,
+                  fontWeight: fontWeight,
+                ),
+              ),
+              onTap: () {
+                tabsRouter.setActiveIndex(item.index);
+              },
+            ),
+          );
+        }).toList(),
+      )),
+    );
+  }
+
+  Widget topSidePanel() {
+    return SliverPadding(
+      padding: const EdgeInsets.only(
+        top: 20.0,
+        bottom: 50.0,
+      ),
+      sliver: SliverList(
+        delegate: SliverChildListDelegate.fixed([
+          IconButton(
+            tooltip: "home".tr(),
+            onPressed: () => context.router.navigate(HomePageRoute()),
+            icon: Opacity(
+              opacity: 0.6,
+              child: Icon(UniconsLine.home),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+
+  Widget fabSidePanel() {
+    return Positioned(
+      left: 40.0,
+      bottom: 20.0,
+      child: ElevatedButton(
+        onPressed: () {
+          appUploadManager.pickImage(context);
+        },
+        style: ElevatedButton.styleFrom(
+          primary: stateColors.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(30.0),
+            ),
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 12.0,
+          ),
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minWidth: 160.0,
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(UniconsLine.upload, color: Colors.white),
+                Padding(padding: const EdgeInsets.only(left: 10.0)),
+                Text(
+                  'Upload',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
