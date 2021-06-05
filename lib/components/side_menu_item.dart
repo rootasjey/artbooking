6abc804a029e171dc39a7ec97e8f86698f032dd9
rootasjey <rootasjey@users.noreply.github.com>
2021-06-06 00:24:21 +0200

    feat(class): :label: add [index] property, remove [destination] one

--- lib/components/side_menu_item.dart
+++ lib/components/side_menu_item.dart
@@ -1,15 +1,14 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 class SideMenuItem {
-  final PageRouteInfo destination;
   final IconData iconData;
+  final int index;
   final String label;
   final Color hoverColor;
 
   const SideMenuItem({
-    @required this.destination,
     @required this.iconData,
+    @required this.index,
     @required this.label,
     @required this.hoverColor,
   });
