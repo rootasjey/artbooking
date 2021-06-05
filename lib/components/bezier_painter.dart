21e16cf76608714507a83fe8dc58f3b20092efe4
rootasjey <rootasjey@users.noreply.github.com>
2021-06-05 16:48:23 +0200

    feat(app): :sparkles: import [BezierPainter] utils from [timelines] package
    Manually import [BezierPainter] utils from [timelines] package source code.

---
+++ lib/components/bezier_painter.dart
@@ -0,0 +1,73 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+/// hardcoded bezier painter
+class BezierPainter extends CustomPainter {
+  const BezierPainter({
+    @required this.color,
+    this.drawStart = true,
+    this.drawEnd = true,
+  });
+
+  final Color color;
+  final bool drawStart;
+  final bool drawEnd;
+
+  Offset _offset(double radius, double angle) {
+    return Offset(
+      radius * cos(angle) + radius,
+      radius * sin(angle) + radius,
+    );
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..style = PaintingStyle.fill
+      ..color = color;
+
+    final radius = size.width / 2;
+
+    var angle;
+    var offset1;
+    var offset2;
+
+    var path;
+
+    if (drawStart) {
+      angle = 3 * pi / 4;
+      offset1 = _offset(radius, angle);
+      offset2 = _offset(radius, -angle);
+      path = Path()
+        ..moveTo(offset1.dx, offset1.dy)
+        ..quadraticBezierTo(0.0, size.height / 2, -radius,
+            radius) // todo: connector start & gradient
+        ..quadraticBezierTo(0.0, size.height / 2, offset2.dx, offset2.dy)
+        ..close();
+
+      canvas.drawPath(path, paint);
+    }
+    if (drawEnd) {
+      angle = -pi / 4;
+      offset1 = _offset(radius, angle);
+      offset2 = _offset(radius, -angle);
+
+      path = Path()
+        ..moveTo(offset1.dx, offset1.dy)
+        ..quadraticBezierTo(size.width, size.height / 2, size.width + radius,
+            radius) // todo: connector end & gradient
+        ..quadraticBezierTo(size.width, size.height / 2, offset2.dx, offset2.dy)
+        ..close();
+
+      canvas.drawPath(path, paint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(BezierPainter oldDelegate) {
+    return oldDelegate.color != color ||
+        oldDelegate.drawStart != drawStart ||
+        oldDelegate.drawEnd != drawEnd;
+  }
+}
