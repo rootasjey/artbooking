379976def34f291aa78031ed8e05f6f46b41a210
rootasjey <rootasjey@users.noreply.github.com>
2021-06-10 23:32:30 +0200

    fix(class): :bug: add missing type for algolia instance

--- lib/utils/search.dart
+++ lib/utils/search.dart
@@ -1,11 +1,14 @@
 import 'package:algolia/algolia.dart';
 import 'package:flutter/foundation.dart';
 
+/// Algolia search helper.
 class SearchHelper {
   static Algolia _algolia;
 
-  static get algolia => _algolia;
+  /// Agolia instance.
+  static Algolia get algolia => _algolia;
 
+  /// Initialize algolia instance.
   static void init({
     @required applicationId,
     @required searchApiKey,
