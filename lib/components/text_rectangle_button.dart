cdd0cacf7a38970ed0ffefff1d1346c1a5771ba6
rootasjey <rootasjey@users.noreply.github.com>
2021-06-28 21:24:38 +0200

    feat(component): :sparkles: add [TextRectangleButton]

---
+++ lib/components/text_rectangle_button.dart
@@ -0,0 +1,41 @@
+import 'package:artbooking/utils/fonts.dart';
+import 'package:flutter/material.dart';
+
+class TextRectangleButton extends StatelessWidget {
+  const TextRectangleButton({
+    Key? key,
+    this.onPressed,
+    required this.icon,
+    required this.label,
+    this.primary,
+  }) : super(key: key);
+
+  final Function()? onPressed;
+  final Widget icon;
+  final Widget label;
+  final Color? primary;
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton.icon(
+      onPressed: onPressed,
+      icon: icon,
+      label: label,
+      style: OutlinedButton.styleFrom(
+        primary: primary,
+        shape: RoundedRectangleBorder(),
+        textStyle: FontsUtils.mainStyle(
+          fontWeight: FontWeight.w600,
+        ),
+        side: BorderSide(
+          width: 2.0,
+          color: Colors.black38.withOpacity(0.2),
+        ),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 28.0,
+          vertical: 18.0,
+        ),
+      ),
+    );
+  }
+}
