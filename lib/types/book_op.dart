014c0034422b44f1dfb450be12f72f4a35be2438
rootasjey <rootasjey@users.noreply.github.com>
2021-06-29 12:04:49 +0200

    style(class): :pencil2: fix typo

--- lib/types/book_op.dart
+++ lib/types/book_op.dart
@@ -1,7 +1,7 @@
 import 'package:artbooking/types/minimal_book_resp.dart';
 
 /// An operation on a book.
-/// It can be adding a book to another data structure, for example.
+/// It can be adding a book to another data structure for example.
 class BookOp {
   final MinimalBookResp book;
   final bool success;
