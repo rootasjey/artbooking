b87c1738296e52f1cf2b5a205323a4c2073c813e
rootasjey <rootasjey@users.noreply.github.com>
2021-07-01 14:15:54 +0200

    feat(component): :sparkles: add [focusNode] property to [ValidationShortcuts]
    The [focusNode] property can be used to force focus request.

--- lib/utils/validation_shortcuts.dart
+++ lib/utils/validation_shortcuts.dart
@@ -2,20 +2,30 @@ import 'package:artbooking/utils/shortcut_intents.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+/// Add keyboard shortcuts events to a [Widget].
+/// Events triggered: [escape], [space], [enter].
+/// Supply a [focusNode] parameter to force focus request
+/// if it doesn't automatically works.
 class ValidationShortcuts extends StatelessWidget {
   const ValidationShortcuts({
     Key? key,
     required this.child,
     this.onValidate,
     this.onCancel,
+    this.focusNode,
   }) : super(key: key);
 
   final Widget child;
   final Function()? onValidate;
   final Function()? onCancel;
+  final FocusNode? focusNode;
 
   @override
   Widget build(BuildContext context) {
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      focusNode?.requestFocus();
+    });
+
     return Shortcuts(
       shortcuts: {
         LogicalKeySet(LogicalKeyboardKey.enter): const EnterIntent(),
@@ -37,6 +47,7 @@ class ValidationShortcuts extends StatelessWidget {
         },
         child: Focus(
           autofocus: true,
+          focusNode: focusNode,
           child: child,
         ),
       ),
