3ab33d2142088b7c31191c8143549b04422b56ee
rootasjey <rootasjey@users.noreply.github.com>
2021-07-16 22:03:25 +0200

    feat: :lipstick: add colors for sections

--- lib/state/colors.dart
+++ lib/state/colors.dart
@@ -29,6 +29,27 @@ abstract class StateColorsBase with Store {
   final Color validation = Color(0xff38d589);
   final Color clairPink = Color(0xFFf5eaf9);
 
+  /// Color for statistics.
+  final Color activity = Colors.red;
+
+  /// Color for books.
+  final Color books = Colors.blue.shade700;
+
+  /// Color for challenges.
+  final Color challenges = Colors.amber;
+
+  /// Color for contests.
+  final Color contests = Colors.indigo;
+
+  /// Color for illustrations.
+  final Color illustrations = Color(0xFF796AD2);
+
+  /// Color for galleries.
+  final Color galleries = Colors.green;
+
+  /// Color for settings.
+  final Color settings = Colors.lime;
+
   ThemeData? themeData;
 
   @action
