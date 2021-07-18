a9b1f4d859f824452ca39c7595e81ab89e8371e5
rootasjey <rootasjey@users.noreply.github.com>
2021-07-18 10:35:56 +0200

    feat(component): :sparkles: add [DashboardSideMenu]
    add [DashboardSideMenu] to sync the state between the selected dashboard sections and the side menu.

---
+++ lib/components/dashboard_side_menu.dart
@@ -0,0 +1,217 @@
+import 'package:artbooking/components/side_menu_item.dart';
+import 'package:artbooking/components/underlined_button.dart';
+import 'package:artbooking/router/locations/dashboard_location.dart';
+import 'package:artbooking/router/locations/home_location.dart';
+import 'package:artbooking/state/colors.dart';
+import 'package:artbooking/utils/constants.dart';
+import 'package:artbooking/utils/fonts.dart';
+import 'package:beamer/beamer.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:unicons/unicons.dart';
+
+/// User's dashboard side menu.
+class DashboardSideMenu extends StatefulWidget {
+  const DashboardSideMenu({
+    Key? key,
+    required this.beamerKey,
+  }) : super(key: key);
+
+  final GlobalKey<BeamerState> beamerKey;
+
+  @override
+  _DashboardSideMenuState createState() => _DashboardSideMenuState();
+}
+
+class _DashboardSideMenuState extends State<DashboardSideMenu> {
+  late BeamerDelegate _beamerDelegate;
+
+  final _sidePanelItems = <SideMenuItem>[
+    SideMenuItem(
+      iconData: UniconsLine.chart_pie,
+      label: "statistics".tr(),
+      hoverColor: stateColors.activity,
+      routePath: DashboardContentLocation.statisticsRoute,
+    ),
+    SideMenuItem(
+      iconData: UniconsLine.picture,
+      label: "illustrations".tr(),
+      hoverColor: stateColors.illustrations,
+      routePath: DashboardContentLocation.illustrationsRoute,
+    ),
+    SideMenuItem(
+      iconData: UniconsLine.book_alt,
+      label: "books".tr(),
+      hoverColor: stateColors.books,
+      routePath: DashboardContentLocation.booksRoute,
+    ),
+    // SideMenuItem(
+    //   destination: MyGalleriesDeepRoute(),
+    //   iconData: UniconsLine.images,
+    //   label: 'Galleries',
+    //   hoverColor: Colors.pink.shade200,
+    // ),
+    // SideMenuItem(
+    //   destination: MyChallengesDeepRoute(),
+    //   iconData: UniconsLine.dumbbell,
+    //   label: 'Challenges',
+    //   hoverColor: Colors.green,
+    // ),
+    // SideMenuItem(
+    //   destination: MyContestsDeepRoute(),
+    //   iconData: UniconsLine.trophy,
+    //   label: 'Contests',
+    //   hoverColor: Colors.yellow.shade800,
+    // ),
+    SideMenuItem(
+      iconData: UniconsLine.setting,
+      label: "settings".tr(),
+      hoverColor: stateColors.settings,
+      routePath: DashboardContentLocation.settingsRoute,
+    ),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _addAdminMenuItems();
+
+    // NOTE: Beamer state isn't ready on 1st frame
+    // probably because [SidePanelMenu] appears before the Beamer widget.
+    // So we use [addPostFrameCallback] to access the state in the next frame.
+    WidgetsBinding.instance?.addPostFrameCallback((timeStamp) {
+      if (widget.beamerKey.currentState != null) {
+        _beamerDelegate = widget.beamerKey.currentState!.routerDelegate;
+        _beamerDelegate.addListener(_setStateListener);
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _beamerDelegate.removeListener(_setStateListener);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (MediaQuery.of(context).size.width < Constants.maxMobileWidth) {
+      return Container();
+    }
+
+    return Container(
+      color: stateColors.lightBackground,
+      width: 300.0,
+      child: CustomScrollView(
+        slivers: <Widget>[
+          topSidePanel(),
+          bodySidePanel(),
+        ],
+      ),
+    );
+  }
+
+  Widget bodySidePanel() {
+    return SliverPadding(
+      padding: const EdgeInsets.only(
+        left: 20.0,
+        right: 20.0,
+      ),
+      sliver: SliverList(
+          delegate: SliverChildListDelegate.fixed(
+        _sidePanelItems.map((sidePanelItem) {
+          Color color = stateColors.foreground.withOpacity(0.6);
+          Color textColor = stateColors.foreground.withOpacity(0.4);
+          FontWeight fontWeight = FontWeight.w600;
+
+          if (context.currentBeamLocation.state.uri.path
+              .contains(sidePanelItem.routePath)) {
+            color = sidePanelItem.hoverColor;
+            textColor = stateColors.foreground.withOpacity(0.6);
+            fontWeight = FontWeight.w700;
+          }
+
+          return Padding(
+            padding: const EdgeInsets.only(
+              left: 24.0,
+              top: 32.0,
+            ),
+            child: UnderlinedButton(
+              leading: Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: Icon(
+                  sidePanelItem.iconData,
+                  color: color,
+                ),
+              ),
+              child: Text(
+                sidePanelItem.label,
+                style: FontsUtils.mainStyle(
+                  color: textColor,
+                  fontSize: 16.0,
+                  fontWeight: fontWeight,
+                ),
+              ),
+              onTap: () {
+                context.beamToNamed(sidePanelItem.routePath);
+                setState(() {});
+              },
+            ),
+          );
+        }).toList(),
+      )),
+    );
+  }
+
+  Widget topSidePanel() {
+    return SliverPadding(
+      padding: const EdgeInsets.only(
+        top: 40.0,
+        bottom: 50.0,
+      ),
+      sliver: SliverList(
+        delegate: SliverChildListDelegate.fixed([
+          IconButton(
+            tooltip: "home".tr(),
+            onPressed: () => context.beamTo(HomeLocation()),
+            icon: Opacity(
+              opacity: 0.6,
+              child: Icon(UniconsLine.home),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+
+  void _addAdminMenuItems() async {
+    // if (!stateUser.canManageQuotes) {
+    //   return;
+    // }
+
+    // _sideMenuItems.addAll([
+    //   SideMenuItem(
+    //     destination: AdminDeepRoute(
+    //       children: [
+    //         AdminTempDeepRoute(
+    //           children: [
+    //             AdminTempQuotesRoute(),
+    //           ],
+    //         )
+    //       ],
+    //     ),
+    //     iconData: UniconsLine.clock_two,
+    //     label: 'Admin Temp Quotes',
+    //     hoverColor: Colors.red,
+    //   ),
+    //   SideMenuItem(
+    //     destination: AdminDeepRoute(children: [QuotidiansRoute()]),
+    //     iconData: UniconsLine.sunset,
+    //     label: 'Quotidians',
+    //     hoverColor: Colors.red,
+    //   ),
+    // ]);
+  }
+
+  void _setStateListener() => setState(() {});
+}
--- lib/screens/dashboard_page.dart
+++ lib/screens/dashboard_page.dart
@@ -1,75 +1,17 @@
-import 'package:artbooking/components/side_menu_item.dart';
-import 'package:artbooking/components/underlined_button.dart';
+import 'package:artbooking/components/dashboard_side_menu.dart';
 import 'package:artbooking/components/upload_window.dart';
 import 'package:artbooking/router/locations/dashboard_location.dart';
-import 'package:artbooking/router/locations/home_location.dart';
-import 'package:artbooking/state/upload_manager.dart';
-import 'package:artbooking/state/colors.dart';
-import 'package:artbooking/utils/constants.dart';
-import 'package:artbooking/utils/fonts.dart';
 import 'package:beamer/beamer.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:unicons/unicons.dart';
 
+/// Widget showing user's dashboard.
 class DashboardPage extends StatefulWidget {
   @override
   _DashboardPageState createState() => _DashboardPageState();
 }
 
 class _DashboardPageState extends State<DashboardPage> {
-  final _sidePanelItems = <SideMenuItem>[
-    SideMenuItem(
-      iconData: UniconsLine.chart_pie,
-      label: "statistics".tr(),
-      hoverColor: stateColors.activity,
-      routePath: DashboardContentLocation.statisticsRoute,
-    ),
-    SideMenuItem(
-      iconData: UniconsLine.picture,
-      label: "illustrations".tr(),
-      hoverColor: stateColors.illustrations,
-      routePath: DashboardContentLocation.illustrationsRoute,
-    ),
-    SideMenuItem(
-      iconData: UniconsLine.book_alt,
-      label: "books".tr(),
-      hoverColor: stateColors.books,
-      routePath: DashboardContentLocation.booksRoute,
-    ),
-    // SideMenuItem(
-    //   destination: MyGalleriesDeepRoute(),
-    //   iconData: UniconsLine.images,
-    //   label: 'Galleries',
-    //   hoverColor: Colors.pink.shade200,
-    // ),
-    // SideMenuItem(
-    //   destination: MyChallengesDeepRoute(),
-    //   iconData: UniconsLine.dumbbell,
-    //   label: 'Challenges',
-    //   hoverColor: Colors.green,
-    // ),
-    // SideMenuItem(
-    //   destination: MyContestsDeepRoute(),
-    //   iconData: UniconsLine.trophy,
-    //   label: 'Contests',
-    //   hoverColor: Colors.yellow.shade800,
-    // ),
-    SideMenuItem(
-      iconData: UniconsLine.setting,
-      label: "settings".tr(),
-      hoverColor: stateColors.settings,
-      routePath: DashboardContentLocation.settingsRoute,
-    ),
-  ];
-
   final _beamerKey = GlobalKey<BeamerState>();
-
-  @override
-  void initState() {
-    super.initState();
-    // tryAddAdminPage();
-  }
 
   @override
   Widget build(context) {
@@ -80,7 +22,9 @@ class _DashboardPageState extends State<DashboardPage> {
           children: [
             Row(
               children: [
-                buildSidePanel(context),
+                DashboardSideMenu(
+                  beamerKey: _beamerKey,
+                ),
                 Expanded(
                   child: Material(
                     elevation: 6.0,
@@ -101,172 +45,6 @@ class _DashboardPageState extends State<DashboardPage> {
               child: UploadWindow(),
             ),
           ],
-        ),
-      ),
-    );
-  }
-
-  Widget buildSidePanel(BuildContext context) {
-    if (MediaQuery.of(context).size.width < Constants.maxMobileWidth) {
-      return Container();
-    }
-
-    return Container(
-      color: stateColors.lightBackground,
-      width: 300.0,
-      child: Stack(
-        children: <Widget>[
-          CustomScrollView(
-            slivers: <Widget>[
-              topSidePanel(),
-              bodySidePanel(),
-            ],
-          ),
-        ],
-      ),
-    );
-  }
-
-  void tryAddAdminPage() async {
-    // if (!stateUser.canManageQuotes) {
-    //   return;
-    // }
-
-    // _sideMenuItems.addAll([
-    //   SideMenuItem(
-    //     destination: AdminDeepRoute(
-    //       children: [
-    //         AdminTempDeepRoute(
-    //           children: [
-    //             AdminTempQuotesRoute(),
-    //           ],
-    //         )
-    //       ],
-    //     ),
-    //     iconData: UniconsLine.clock_two,
-    //     label: 'Admin Temp Quotes',
-    //     hoverColor: Colors.red,
-    //   ),
-    //   SideMenuItem(
-    //     destination: AdminDeepRoute(children: [QuotidiansRoute()]),
-    //     iconData: UniconsLine.sunset,
-    //     label: 'Quotidians',
-    //     hoverColor: Colors.red,
-    //   ),
-    // ]);
-  }
-
-  Widget bodySidePanel() {
-    return SliverPadding(
-      padding: const EdgeInsets.only(
-        left: 20.0,
-        right: 20.0,
-      ),
-      sliver: SliverList(
-          delegate: SliverChildListDelegate.fixed(
-        _sidePanelItems.map((sidePanelItem) {
-          Color color = stateColors.foreground.withOpacity(0.6);
-          Color textColor = stateColors.foreground.withOpacity(0.4);
-          FontWeight fontWeight = FontWeight.w600;
-
-          if (context.currentBeamLocation.state.uri.path
-              .contains(sidePanelItem.routePath)) {
-            color = sidePanelItem.hoverColor;
-            textColor = stateColors.foreground.withOpacity(0.6);
-            fontWeight = FontWeight.w700;
-          }
-
-          return Padding(
-            padding: const EdgeInsets.only(
-              left: 24.0,
-              top: 32.0,
-            ),
-            child: UnderlinedButton(
-              leading: Padding(
-                padding: const EdgeInsets.only(right: 16.0),
-                child: Icon(
-                  sidePanelItem.iconData,
-                  color: color,
-                ),
-              ),
-              child: Text(
-                sidePanelItem.label,
-                style: FontsUtils.mainStyle(
-                  color: textColor,
-                  fontSize: 16.0,
-                  fontWeight: fontWeight,
-                ),
-              ),
-              onTap: () {
-                context.beamToNamed(sidePanelItem.routePath);
-                setState(() {});
-              },
-            ),
-          );
-        }).toList(),
-      )),
-    );
-  }
-
-  Widget topSidePanel() {
-    return SliverPadding(
-      padding: const EdgeInsets.only(
-        top: 40.0,
-        bottom: 50.0,
-      ),
-      sliver: SliverList(
-        delegate: SliverChildListDelegate.fixed([
-          IconButton(
-            tooltip: "home".tr(),
-            onPressed: () => context.beamTo(HomeLocation()),
-            icon: Opacity(
-              opacity: 0.6,
-              child: Icon(UniconsLine.home),
-            ),
-          ),
-        ]),
-      ),
-    );
-  }
-
-  Widget fabSidePanel() {
-    return Positioned(
-      left: 40.0,
-      bottom: 20.0,
-      child: ElevatedButton(
-        onPressed: () {
-          appUploadManager.pickImage(context);
-        },
-        style: ElevatedButton.styleFrom(
-          primary: stateColors.primary,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(
-              Radius.circular(30.0),
-            ),
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 12.0,
-          ),
-          child: ConstrainedBox(
-            constraints: BoxConstraints(
-              minWidth: 160.0,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Icon(UniconsLine.upload, color: Colors.white),
-                Padding(padding: const EdgeInsets.only(left: 10.0)),
-                Text(
-                  'Upload',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-          ),
         ),
       ),
     );
