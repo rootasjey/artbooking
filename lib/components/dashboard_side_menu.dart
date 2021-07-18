812c059fb8c1182f0bd03823d45b072b08ec0235
rootasjey <rootasjey@users.noreply.github.com>
2021-07-18 15:00:23 +0200

    feat(component): :sparkles: add ability to collapse the left dashboard panel (menu items)

--- lib/components/dashboard_side_menu.dart
+++ lib/components/dashboard_side_menu.dart
@@ -8,6 +8,7 @@ import 'package:artbooking/utils/fonts.dart';
 import 'package:beamer/beamer.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:supercharged/supercharged.dart';
 import 'package:unicons/unicons.dart';
 
 /// User's dashboard side menu.
@@ -25,6 +26,13 @@ class DashboardSideMenu extends StatefulWidget {
 
 class _DashboardSideMenuState extends State<DashboardSideMenu> {
   late BeamerDelegate _beamerDelegate;
+
+  /// True if the side menu is expanded showing icons and labels.
+  /// If false, the side menu shows only icon.
+  /// Default to true.
+  bool _isExpanded = true;
+
+  double _paneWidth = 300.0;
 
   final _sidePanelItems = <SideMenuItem>[
     SideMenuItem(
@@ -99,22 +107,36 @@ class _DashboardSideMenuState extends State<DashboardSideMenu> {
       return Container();
     }
 
-    return Container(
+    return Material(
       color: stateColors.lightBackground,
-      width: 300.0,
-      child: CustomScrollView(
-        slivers: <Widget>[
-          topSidePanel(),
-          bodySidePanel(),
-        ],
+      child: AnimatedContainer(
+        duration: 500.milliseconds,
+        curve: Curves.easeOutExpo,
+        width: _paneWidth,
+        child: Stack(
+          children: [
+            OverflowBox(
+              minWidth: 40.0,
+              maxWidth: 300.0,
+              alignment: Alignment.topLeft,
+              child: CustomScrollView(
+                slivers: <Widget>[
+                  topSidePanel(),
+                  bodySidePanel(),
+                ],
+              ),
+            ),
+            toggleExpandButton(),
+          ],
+        ),
       ),
     );
   }
 
   Widget bodySidePanel() {
     return SliverPadding(
-      padding: const EdgeInsets.only(
-        left: 20.0,
+      padding: EdgeInsets.only(
+        left: _isExpanded ? 20.0 : 16.0,
         right: 20.0,
       ),
       sliver: SliverList(
@@ -131,31 +153,37 @@ class _DashboardSideMenuState extends State<DashboardSideMenu> {
             fontWeight = FontWeight.w700;
           }
 
-          return Padding(
-            padding: const EdgeInsets.only(
-              left: 24.0,
-              top: 32.0,
-            ),
-            child: UnderlinedButton(
-              leading: Padding(
-                padding: const EdgeInsets.only(right: 16.0),
-                child: Icon(
-                  sidePanelItem.iconData,
-                  color: color,
-                ),
+          return Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: EdgeInsets.only(
+                left: _isExpanded ? 24.0 : 0.0,
+                top: 32.0,
               ),
-              child: Text(
-                sidePanelItem.label,
-                style: FontsUtils.mainStyle(
-                  color: textColor,
-                  fontSize: 16.0,
-                  fontWeight: fontWeight,
+              child: UnderlinedButton(
+                compact: !_isExpanded,
+                tooltip: sidePanelItem.label,
+                hoverColor: Colors.transparent,
+                leading: Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: Icon(
+                    sidePanelItem.iconData,
+                    color: color,
+                  ),
                 ),
+                child: Text(
+                  sidePanelItem.label,
+                  style: FontsUtils.mainStyle(
+                    color: textColor,
+                    fontSize: 16.0,
+                    fontWeight: fontWeight,
+                  ),
+                ),
+                onTap: () {
+                  context.beamToNamed(sidePanelItem.routePath);
+                  setState(() {});
+                },
               ),
-              onTap: () {
-                context.beamToNamed(sidePanelItem.routePath);
-                setState(() {});
-              },
             ),
           );
         }).toList(),
@@ -163,21 +191,46 @@ class _DashboardSideMenuState extends State<DashboardSideMenu> {
     );
   }
 
+  Widget toggleExpandButton() {
+    return Positioned(
+      bottom: 24.0,
+      left: _isExpanded ? 32.0 : 16.0,
+      child: Opacity(
+        opacity: 0.6,
+        child: IconButton(
+          tooltip: _isExpanded ? "collapse".tr() : "expand".tr(),
+          icon: _isExpanded
+              ? Icon(UniconsLine.left_arrow_from_left)
+              : Icon(UniconsLine.arrow_from_right),
+          onPressed: _toggleSideMenu,
+        ),
+      ),
+    );
+  }
+
   Widget topSidePanel() {
     return SliverPadding(
-      padding: const EdgeInsets.only(
+      padding: EdgeInsets.only(
         top: 40.0,
         bottom: 50.0,
+        left: _isExpanded ? 0.0 : 16.0,
       ),
       sliver: SliverList(
         delegate: SliverChildListDelegate.fixed([
-          IconButton(
-            tooltip: "home".tr(),
-            onPressed: () => context.beamTo(HomeLocation()),
-            icon: Opacity(
-              opacity: 0.6,
-              child: Icon(UniconsLine.home),
-            ),
+          Column(
+            crossAxisAlignment: _isExpanded
+                ? CrossAxisAlignment.center
+                : CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                tooltip: "home".tr(),
+                onPressed: () => context.beamTo(HomeLocation()),
+                icon: Opacity(
+                  opacity: 0.6,
+                  child: Icon(UniconsLine.home),
+                ),
+              ),
+            ],
           ),
         ]),
       ),
@@ -214,4 +267,11 @@ class _DashboardSideMenuState extends State<DashboardSideMenu> {
   }
 
   void _setStateListener() => setState(() {});
+
+  void _toggleSideMenu() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+      _paneWidth = _isExpanded ? 300.0 : 70.0;
+    });
+  }
 }
