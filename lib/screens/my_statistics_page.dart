ec2b77e7ad9860dc06a9ae525f60dcf2c4e1f8a2
rootasjey <rootasjey@users.noreply.github.com>
2021-07-28 16:07:42 +0200

    feat(page): :sparkles: add data listener on user's stats, and add shimmer loading
    Add snapshot listener on the current auth user. Add shimmer components when loading data on [MyStatisticsPage].

--- lib/screens/my_statistics_page.dart
+++ lib/screens/my_statistics_page.dart
@@ -1,13 +1,17 @@
-import 'package:artbooking/components/full_page_loading.dart';
+import 'dart:async';
+
 import 'package:artbooking/components/main_app_bar.dart';
+import 'package:artbooking/components/shimmer_card.dart';
 import 'package:artbooking/components/sliver_edge_padding.dart';
 import 'package:artbooking/components/square_stats.dart';
 import 'package:artbooking/router/locations/dashboard_location.dart';
-import 'package:artbooking/screens/signin_page.dart';
 import 'package:artbooking/state/colors.dart';
 import 'package:artbooking/state/user.dart';
+import 'package:artbooking/types/user/user_firestore.dart';
+import 'package:artbooking/utils/app_logger.dart';
 import 'package:artbooking/utils/fonts.dart';
 import 'package:beamer/beamer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -20,14 +24,22 @@ class MyActivityPage extends StatefulWidget {
 }
 
 class _MyActivityPageState extends State<MyActivityPage> {
+  bool _isLoading = false;
+
   final _scrollController = ScrollController();
 
-  bool isLoading = false;
+  StreamSubscription<DocumentSnapshot<Map<String, dynamic>>>? _userSnapListener;
 
   @override
   void initState() {
     super.initState();
     fetch();
+  }
+
+  @override
+  dispose() {
+    _userSnapListener?.cancel();
+    super.dispose();
   }
 
   @override
@@ -46,7 +58,7 @@ class _MyActivityPageState extends State<MyActivityPage> {
   }
 
   Widget body() {
-    if (isLoading) {
+    if (_isLoading) {
       return loadingView();
     }
 
@@ -65,16 +77,6 @@ class _MyActivityPageState extends State<MyActivityPage> {
         ),
       ]),
     );
-  }
-
-  Widget loadingView() {
-    return SliverList(
-        delegate: SliverChildListDelegate([
-      Center(
-        child: FullPageLoading(),
-      ),
-      Padding(padding: const EdgeInsets.only(bottom: 200.0)),
-    ]));
   }
 
   Widget header() {
@@ -101,6 +103,59 @@ class _MyActivityPageState extends State<MyActivityPage> {
           ),
         ]),
       ),
+    );
+  }
+
+  Widget loadingSquareStatsList() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 50.0,
+        right: 50.0,
+        top: 20.0,
+      ),
+      child: Wrap(
+        spacing: 16.0,
+        runSpacing: 16.0,
+        alignment: WrapAlignment.start,
+        children: <Widget>[
+          ShimmerCard(),
+          ShimmerCard(),
+          ShimmerCard(),
+          ShimmerCard(),
+        ],
+      ),
+    );
+  }
+
+  Widget loadingTextStatsRow() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 50.0,
+        right: 50.0,
+        top: 60.0,
+        bottom: 100.0,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          ShimmerCard(height: 40.0, width: 300.0, elevation: 1.0),
+          ShimmerCard(height: 40.0, width: 300.0, elevation: 1.0),
+        ],
+      ),
+    );
+  }
+
+  Widget loadingView() {
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        loadingSquareStatsList(),
+        loadingTextStatsRow(),
+        Padding(
+          padding: const EdgeInsets.only(
+            bottom: 200.0,
+          ),
+        ),
+      ]),
     );
   }
 
@@ -251,32 +306,44 @@ class _MyActivityPageState extends State<MyActivityPage> {
   }
 
   void fetch() async {
-    setState(() {
-      isLoading = true;
-    });
+    setState(() => _isLoading = true);
 
     try {
       final userAuth = FirebaseAuth.instance.currentUser;
 
       if (userAuth == null) {
-        WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (_) => SigninPage()),
-          );
-        });
-
+        setState(() => _isLoading = false);
         return;
       }
 
-      setState(() {
-        isLoading = false;
-      });
-    } catch (err) {
-      debugPrint(err.toString());
+      final snapshot =
+          FirebaseFirestore.instance.collection('users').doc(userAuth.uid);
 
-      setState(() {
-        isLoading = false;
+      if (_userSnapListener != null) {
+        _userSnapListener?.cancel();
+      }
+
+      _userSnapListener = snapshot.snapshots().listen((event) {
+        final data = event.data();
+
+        if (data == null) {
+          return;
+        }
+
+        data['id'] = event.id;
+
+        setState(() {
+          stateUser.userFirestore = UserFirestore.fromJSON(data);
+        });
+      }, onError: (error) {
+        appLogger.e(error);
+      }, onDone: () {
+        _userSnapListener?.cancel();
       });
+    } catch (error) {
+      appLogger.e(error);
+    } finally {
+      setState(() => _isLoading = false);
     }
   }
 
