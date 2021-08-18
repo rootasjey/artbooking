5faf2a3ce73f6114e5e7273193ebe0a87cfb248c
rootasjey <rootasjey@users.noreply.github.com>
2021-08-18 22:26:30 +0200

    feat(class): :sparkles: add `createdAt` to [BookIllustration]

--- lib/types/book_illustration.dart
+++ lib/types/book_illustration.dart
@@ -1,18 +1,25 @@
 import 'package:artbooking/types/v_scale_factor.dart';
+import 'package:artbooking/utils/date_helper.dart';
 
 class BookIllustration {
-  /// Firesotre id.
+  /// Illustration's id.
   final String id;
 
+  /// When this illustration was added to the parent book.
+  final DateTime createdAt;
+
+  /// Defines this illustration's side inside this book.
   VScaleFactor vScaleFactor;
 
   BookIllustration({
+    required this.createdAt,
     required this.id,
     required this.vScaleFactor,
   });
 
   factory BookIllustration.fromJSON(Map<String, dynamic> data) {
     return BookIllustration(
+      createdAt: DateHelper.fromFirestore(data['createdAt']),
       id: data['id'],
       vScaleFactor: VScaleFactor.fromJSON(data['vScaleFactor']),
     );
