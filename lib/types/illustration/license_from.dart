ba77cdafc8e1d6e1f48bf63377bd056a0d0d628a
rootasjey <jeremie@rootasjey.dev>
2022-01-16 04:33:10 +0100

    feat: :label: add `LicenseFrom` enum

---
+++ lib/types/illustration/license_from.dart
@@ -0,0 +1,11 @@
+/// Tell if the license has been created by a staff member,
+/// or is local to an user.
+enum LicenseFrom {
+  /// License created by a staff member.
+  /// All users have access to it.
+  staff,
+
+  /// License created by an iser.
+  /// Only them can use it.
+  user,
+}
