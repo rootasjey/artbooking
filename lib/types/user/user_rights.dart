7471d162c16f892d4033bea41416018e609f27ef
rootasjey <jeremie@rootasjey.dev>
2022-01-16 16:57:06 +0100

    feat: :label: add `canManageLicense` to `UserRights`

--- lib/types/user/user_rights.dart
+++ lib/types/user/user_rights.dart
@@ -1,9 +1,15 @@
 /// Define which actions are available for the target user.
 class UserRights {
-  const UserRights({this.isAdmin = false});
+  const UserRights({
+    this.isAdmin = false,
+    this.canManageLicense = false,
+  });
 
-  /// If true, the current authenticated user is an admin.
+  /// If true, the current user is an admin.
   final bool isAdmin;
+
+  /// True if the current user can manage (add, remove, edit) staff licenses.
+  final bool canManageLicense;
 
   factory UserRights.fromJSON(Map<String, dynamic>? data) {
     if (data == null) {
@@ -12,12 +18,14 @@ class UserRights {
 
     return UserRights(
       isAdmin: data['user:admin'] ?? false,
+      canManageLicense: data['user:managelicense'] ?? false,
     );
   }
 
   Map<String, dynamic> toJSON() {
     Map<String, dynamic> data = Map();
     data['user:admin'] = isAdmin;
+    data['user:managelicense'] = canManageLicense;
 
     return data;
   }
