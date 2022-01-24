0046e61b1d4fc77cad00bed633861d130d616ade
rootasjey <jeremie@rootasjey.dev>
2022-01-24 15:27:49 +0100

    feat: :lipstick: update undefined page UI

--- lib/screens/undefined_page.dart
+++ lib/screens/undefined_page.dart
@@ -3,6 +3,7 @@ import 'package:artbooking/components/application_bar/application_bar.dart';
 import 'package:artbooking/globals/utilities.dart';
 import 'package:artbooking/router/locations/home_location.dart';
 import 'package:beamer/beamer.dart';
+import 'package:easy_localization/src/public_ext.dart';
 import 'package:flutter/material.dart';
 import 'package:unicons/unicons.dart';
 
@@ -14,132 +15,108 @@ class UndefinedPage extends StatefulWidget {
 class _UndefinedPageState extends State<UndefinedPage> {
   @override
   Widget build(BuildContext context) {
+    String location = '';
+    final int length = context.beamingHistory.length;
+
+    if (length > 1) {
+      final beamLocation = context.beamingHistory.elementAt(length - 2);
+      location = beamLocation.state.routeInformation.location ?? "";
+    }
+
     return Scaffold(
       body: CustomScrollView(
         slivers: [
           ApplicationBar(),
-          SliverPadding(
-            padding: const EdgeInsets.only(top: 60.0),
-            sliver: SliverList(
-              delegate: SliverChildListDelegate.fixed([
-                Column(
-                  children: <Widget>[
-                    title(),
-                    subtitle(),
-                    illustration(),
-                    navButton(),
-                    quoteCard(),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 300.0),
-                    ),
-                  ],
-                ),
-              ]),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget quoteCard() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: SizedBox(
-        width: 400.0,
-        child: Card(
-            elevation: 2,
-            child: Padding(
-              padding: const EdgeInsets.all(40.0),
-              child: Column(
+          SliverList(
+            delegate: SliverChildListDelegate.fixed([
+              Column(
                 children: <Widget>[
-                  Opacity(
-                    opacity: 0.8,
+                  Padding(
+                    padding: const EdgeInsets.only(top: 60.0, bottom: 8.0),
                     child: Text(
-                      // 'It is by getting lost that we learn.',
-                      'When we are lost, what matters is to find our way back.',
-                      style: Utilities.fonts.style(
-                        fontSize: 18.0,
+                      '404',
+                      style: TextStyle(
+                        fontSize: 120.0,
                         fontWeight: FontWeight.w600,
                       ),
                     ),
                   ),
+                  Opacity(
+                    opacity: 0.6,
+                    child: RichText(
+                      text: TextSpan(
+                        text: "undefined_page_route_prefix".tr(),
+                        style: Utilities.fonts.style(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.w700,
+                          color: Theme.of(context).textTheme.bodyText1?.color,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: location,
+                            style: Utilities.fonts.style(
+                              fontWeight: FontWeight.w700,
+                              color: Theme.of(context).secondaryHeaderColor,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "undefined_page_route_suffix".tr(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0, bottom: 12.0),
+                    child: Icon(
+                      UniconsLine.car_sideview,
+                      size: 40.0,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 24.0, bottom: 24.0),
+                    child: DarkElevatedButton.large(
+                      onPressed: () => context.beamToNamed(HomeLocation.route),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text("undefined_page_get_back".tr()),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 400.0,
+                    padding: const EdgeInsets.only(
+                      top: 8.0,
+                      left: 8.0,
+                      right: 8.0,
+                      bottom: 300,
+                    ),
+                    child: Card(
+                        elevation: 4.0,
+                        color: Theme.of(context).backgroundColor,
+                        child: Padding(
+                          padding: const EdgeInsets.all(40.0),
+                          child: Column(
+                            children: <Widget>[
+                              Opacity(
+                                opacity: 0.8,
+                                child: Text(
+                                  "undefined_page_quote".tr(),
+                                  style: Utilities.fonts.style(
+                                    fontSize: 18.0,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )),
+                  ),
                 ],
               ),
-            )),
-      ),
-    );
-  }
-
-  Widget illustration() {
-    return Padding(
-      padding: const EdgeInsets.only(
-        top: 12.0,
-        bottom: 12.0,
-      ),
-      child: Icon(
-        UniconsLine.car_sideview,
-        size: 40.0,
-      ),
-    );
-  }
-
-  Widget navButton() {
-    return Padding(
-      padding: const EdgeInsets.only(top: 24.0, bottom: 24.0),
-      child: DarkElevatedButton.large(
-        onPressed: () => context.beamToNamed(HomeLocation.route),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text('Return on the way'),
-        ),
-      ),
-    );
-  }
-
-  Widget subtitle() {
-    final String location = Beamer.of(context)
-            .currentBeamLocation
-            .state
-            .routeInformation
-            .location ??
-        '';
-
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Opacity(
-        opacity: 0.6,
-        child: RichText(
-          text: TextSpan(
-            text: 'Route for ',
-            style: TextStyle(
-              fontSize: 18.0,
-              color: Theme.of(context).textTheme.bodyText1?.color,
-            ),
-            children: [
-              TextSpan(
-                text: location,
-                style: TextStyle(
-                  color: Theme.of(context).secondaryHeaderColor,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              TextSpan(
-                text: ' is not defined.',
-              ),
-            ],
+            ]),
           ),
-        ),
-      ),
-    );
-  }
-
-  Widget title() {
-    return Text(
-      '404',
-      style: TextStyle(
-        fontSize: 120.0,
-        fontWeight: FontWeight.w600,
+        ],
       ),
     );
   }
