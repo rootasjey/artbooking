181baa448f974ba2f67d2b958bcec7ac027b9ad9
rootasjey <jeremie@rootasjey.dev>
2022-01-30 00:10:11 +0100

    feat: :lipstick: add new constant colors

--- lib/globals/constants/colors_constants.dart
+++ lib/globals/constants/colors_constants.dart
@@ -1,4 +1,3 @@
-import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 
 class ColorsConstants {
@@ -6,18 +5,6 @@ class ColorsConstants {
   final Color activity = Colors.red;
 
   final Color clairPink = Color(0xFFf5eaf9);
-
-  final Color dark = Color(0xFF303030);
-  final Color lightBackground = Color(0xFfe3e6ec);
-
-  /// Color for posts.
-  final Color posts = Colors.blue.shade700;
-
-  /// Primary application's color.
-  final Color primary = Color(0xFF796AD2);
-
-  /// Color for projects.
-  final Color projects = Colors.amber;
 
   /// Color for books.
   final Color books = Colors.blue.shade700;
@@ -28,36 +15,39 @@ class ColorsConstants {
   /// Color for contests.
   final Color contests = Colors.indigo;
 
-  /// Color for illustrations.
-  final Color illustrations = Color(0xFF796AD2);
+  final Color dark = Color(0xFF303030);
+
+  final Color delete = Colors.pink;
+
+  /// Color for email.
+  final Color email = Colors.blue.shade300;
 
   /// Color for galleries.
   final Color galleries = Colors.green;
 
-  /// Color for settings.
-  final Color settings = Colors.teal;
+  /// Color for illustrations.
+  final Color illustrations = Color(0xFF796AD2);
+  final Color lightBackground = Color(0xFfe3e6ec);
+  final Color location = Colors.green.shade300;
+
+  final Color password = Colors.orange.shade300;
+
+  /// Primary application's color.
+  final Color primary = Color(0xFF796AD2);
 
   /// Color for profile.
   final Color profile = Colors.indigo;
+
+  /// Color for projects.
+  final Color projects = Colors.amber;
+
+  /// Color for settings.
+  final Color settings = Colors.teal;
+
+  final Color summary = Colors.deepPurple.shade300;
 
   /// Secondary application's color.
   Color secondary = Colors.pink;
 
   final Color validation = Colors.green;
-
-  Color getBackgroundColor(BuildContext context) {
-    if (AdaptiveTheme.of(context).brightness == Brightness.dark) {
-      return dark;
-    }
-
-    return lightBackground;
-  }
-
-  Color getForeground(BuildContext context) {
-    if (AdaptiveTheme.of(context).brightness == Brightness.dark) {
-      return Colors.white;
-    }
-
-    return Colors.black;
-  }
 }
