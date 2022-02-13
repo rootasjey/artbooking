83af492f08b15ae327519a14c8304ffbf625b5b6
rootasjey <jeremie@rootasjey.dev>
2022-02-13 12:27:55 +0100

    feat: :label: add `like`, `unlike` in book actions

--- lib/types/enums/enum_book_item_action.dart
+++ lib/types/enums/enum_book_item_action.dart
@@ -3,8 +3,14 @@ enum EnumBookItemAction {
   /// Delete this book from database.
   delete,
 
+  /// Like a book.
+  like,
+
   /// Rename book's title.
   rename,
+
+  /// Unlike a book.
+  unlike,
 
   /// Update properties of this book.
   update,
