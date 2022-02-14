dd2df56d949e84836a9be184255cf08456df97a3
rootasjey <jeremie@rootasjey.dev>
2022-02-14 20:26:49 +0100

    feat: :sparkles: update `SquareButton` component
    Prevent null tooltip `message` bug. Add `active` property to change square background color.

--- lib/components/buttons/square_button.dart
+++ lib/components/buttons/square_button.dart
@@ -7,33 +7,42 @@ class SquareButton extends StatelessWidget {
     this.message,
     this.onTap,
     this.opacity = 0.4,
+    this.active = false,
   }) : super(key: key);
 
   final String? message;
   final void Function()? onTap;
   final Widget child;
   final double opacity;
+  final bool active;
 
   @override
   Widget build(BuildContext context) {
-    return Tooltip(
-      message: message,
-      child: InkWell(
-        onTap: onTap,
-        child: Opacity(
-          opacity: opacity,
-          child: Container(
-            padding: const EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-              border: Border.all(
-                width: 2.0,
-                color: Colors.black54,
-              ),
+    final _childWidget = InkWell(
+      onTap: onTap,
+      child: Opacity(
+        opacity: opacity,
+        child: Container(
+          padding: const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 2.0,
+              color: active ? Colors.white54 : Colors.black54,
             ),
-            child: child,
+            color: active ? Theme.of(context).primaryColor : null,
           ),
+          child: child,
         ),
       ),
     );
+
+    if (message != null) {
+      return Tooltip(
+        message: message,
+        child: _childWidget,
+      );
+    }
+
+    return _childWidget;
   }
 }
