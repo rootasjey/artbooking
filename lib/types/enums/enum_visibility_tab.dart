de3257a43d38c8f370afc391756ec62217f10287
rootasjey <jeremie@rootasjey.dev>
2022-02-15 15:25:54 +0100

    feat: :label: add `EnumVisibilityTab`

---
+++ lib/types/enums/enum_visibility_tab.dart
@@ -0,0 +1,9 @@
+/// Choose whetheir to display active content (books, illustrations)
+/// or archived ones.
+enum EnumVisibilityTab {
+  /// Active content either visible by the owner or other users.
+  active,
+
+  /// Archived content only visible by the owner in a dedicated section.
+  archived,
+}
