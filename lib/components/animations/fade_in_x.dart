05e39ae2acdbc41f0fcd799d9e0aeef03d232e93
rootasjey <jeremie@rootasjey.dev>
2022-02-22 18:37:07 +0100

    feat: :dizzy: reduce animation duration & add it as a ext. prop.

--- lib/components/animations/fade_in_x.dart
+++ lib/components/animations/fade_in_x.dart
@@ -5,25 +5,25 @@ import 'package:supercharged/supercharged.dart';
 
 /// Animate translateX and opacity of a child widget.
 class FadeInX extends StatelessWidget {
-  final Duration delay;
-  final Widget? child;
-
-  final double beginX;
-  final double endX;
-
   FadeInX({
     this.beginX = 0.0,
     this.child,
     this.delay = const Duration(seconds: 0),
     this.endX = 0.0,
+    this.duration = const Duration(milliseconds: 300),
   });
+
+  final double beginX;
+  final double endX;
+  final Duration delay;
+  final Duration duration;
+  final Widget? child;
 
   @override
   Widget build(BuildContext context) {
     final tween = MultiTween<EnumAniProps>()
-      ..add(EnumAniProps.opacity, 0.0.tweenTo(1.0), 500.milliseconds)
-      ..add(EnumAniProps.translateX, Tween(begin: beginX, end: endX),
-          500.milliseconds);
+      ..add(EnumAniProps.opacity, 0.0.tweenTo(1.0), duration)
+      ..add(EnumAniProps.translateX, Tween(begin: beginX, end: endX), duration);
 
     return PlayAnimation<MultiTweenValues<EnumAniProps>>(
       tween: tween,
--- lib/components/animations/fade_in_y.dart
+++ lib/components/animations/fade_in_y.dart
@@ -5,25 +5,25 @@ import 'package:supercharged/supercharged.dart';
 
 /// Animate translateY and opacity of a child widget.
 class FadeInY extends StatelessWidget {
-  final Duration delay;
-  final Widget? child;
-
-  final double beginY;
-  final double endY;
-
   FadeInY({
     this.beginY = 0.0,
     this.child,
     this.delay = const Duration(seconds: 0),
     this.endY = 0.0,
+    this.duration = const Duration(milliseconds: 300),
   });
+
+  final double beginY;
+  final double endY;
+  final Duration delay;
+  final Duration duration;
+  final Widget? child;
 
   @override
   Widget build(BuildContext context) {
     final tween = MultiTween<EnumAniProps>()
-      ..add(EnumAniProps.opacity, 0.0.tweenTo(1.0), 500.milliseconds)
-      ..add(EnumAniProps.translateY, Tween(begin: beginY, end: endY),
-          500.milliseconds);
+      ..add(EnumAniProps.opacity, 0.0.tweenTo(1.0), duration)
+      ..add(EnumAniProps.translateY, Tween(begin: beginY, end: endY), duration);
 
     return PlayAnimation<MultiTweenValues<EnumAniProps>>(
       tween: tween,
