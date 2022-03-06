d3f4fbcc38bfa2b6d9c12f9f5f17114bf902436d
rootasjey <jeremie@rootasjey.dev>
2022-03-06 12:33:08 +0100

    feat: :label: add `EnumSelectType`
    Used to know when to add new items or replace them after a selection (e.g. selecting illustrations).

---
+++ lib/types/enums/enum_select_type.dart
@@ -0,0 +1,8 @@
+/// Define how to handle selected content compared to existing content.
+enum EnumSelectType {
+  /// Selected content will be added to existing one.
+  add,
+
+  /// Selected content will replace existing one.
+  replace,
+}
