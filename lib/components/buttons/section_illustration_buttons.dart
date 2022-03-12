12464c381fb0bdcdb2f36b7f234971a8bb4065a0
rootasjey <jeremie@rootasjey.dev>
2022-03-12 10:53:18 +0100

    feat: :sparkles: add `SectionIllustrationButtons` component
    Buttons to replace or remove current section's illustration.

---
+++ lib/components/buttons/section_illustration_buttons.dart
@@ -0,0 +1,39 @@
+import 'package:artbooking/components/buttons/circle_button.dart';
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:unicons/unicons.dart';
+
+class SectionIllustrationButtons extends StatelessWidget {
+  const SectionIllustrationButtons({
+    Key? key,
+    this.onRemoveIllustration,
+    this.onPickIllustration,
+  }) : super(key: key);
+
+  final void Function()? onRemoveIllustration;
+  final void Function()? onPickIllustration;
+
+  @override
+  Widget build(BuildContext context) {
+    return Wrap(
+      spacing: 12.0,
+      runSpacing: 12.0,
+      children: [
+        CircleButton(
+          elevation: 2.0,
+          showBorder: true,
+          onTap: onRemoveIllustration,
+          tooltip: "illustration_remove".tr(),
+          icon: Icon(UniconsLine.minus),
+        ),
+        CircleButton(
+          elevation: 2.0,
+          showBorder: true,
+          onTap: onPickIllustration,
+          tooltip: "illustration_change".tr(),
+          icon: Icon(UniconsLine.exchange),
+        ),
+      ],
+    );
+  }
+}
