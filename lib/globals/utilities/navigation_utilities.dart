6d7611eac13f2451d3432055479688f8a8bc4605
rootasjey <jeremie@rootasjey.dev>
2022-03-16 01:22:49 +0100

    feat: :sparkles: add `getHeroTag(...)`, `profileToIllustration(...)`
    Add new methods to `NavigationUtilities`.

--- lib/globals/utilities/navigation_utilities.dart
+++ lib/globals/utilities/navigation_utilities.dart
@@ -1,3 +1,6 @@
+import 'package:artbooking/router/locations/atelier_location.dart';
+import 'package:artbooking/router/navigation_state_helper.dart';
+import 'package:artbooking/types/illustration/illustration.dart';
 import 'package:beamer/beamer.dart';
 import 'package:flutter/widgets.dart';
 
@@ -11,5 +14,32 @@ class NavigationUtilities {
     }
 
     Beamer.of(context).popRoute();
+  }
+
+  /// Return hero tag stored as a string from `routeState` map if any.
+  String getHeroTag(Object? routeState) {
+    if (routeState == null) {
+      return "";
+    }
+
+    final mapState = routeState as Map<String, dynamic>;
+    return mapState["heroTag"] ?? "";
+  }
+
+  /// Navigate from a profile page to an illustration page.
+  void profileToIllustration(
+    BuildContext context, {
+    required Illustration illustration,
+    required String heroTag,
+  }) {
+    NavigationStateHelper.illustration = illustration;
+    Beamer.of(context).beamToNamed(
+      AtelierLocationContent.profileIllustrationRoute.replaceFirst(
+        ":illustrationId",
+        illustration.id,
+      ),
+      data: {"illustrationId": illustration.id},
+      routeState: {"heroTag": heroTag},
+    );
   }
 }
