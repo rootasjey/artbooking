526774f2df110d8debf5c66a1d2cdd4db54995da
rootasjey <jeremie@rootasjey.dev>
2022-03-17 10:37:08 +0100

    feat: :sparkles: add default image for book cover
    Add default image link for `BookCover.empty(...)`

--- lib/types/book/book_cover.dart
+++ lib/types/book/book_cover.dart
@@ -17,7 +17,9 @@ class BookCover {
   factory BookCover.empty() {
     return BookCover(
       mode: BookCoverMode.lastIllustrationAdded,
-      link: '',
+      link: "https://firebasestorage.googleapis.com/v0/b/"
+          "artbooking-54d22.appspot.com/o/static%2Fimages%2Fbook_cover_512x683.png"
+          "?alt=media&token=d77bc23b-90d7-4663-be3a-e878c6403e51",
       updatedAt: DateTime.now(),
     );
   }
@@ -28,19 +30,19 @@ class BookCover {
     }
 
     return BookCover(
-      link: data['link'],
-      updatedAt: Utilities.date.fromFirestore(data['updated_at']),
-      mode: parseBookCoverMode(data['mode']),
+      link: data["link"],
+      updatedAt: Utilities.date.fromFirestore(data["updated_at"]),
+      mode: parseBookCoverMode(data["mode"]),
     );
   }
 
   static BookCoverMode parseBookCoverMode(String rawMode) {
     switch (rawMode) {
-      case 'last_illustration_added':
+      case "last_illustration_added":
         return BookCoverMode.lastIllustrationAdded;
-      case 'chosen_illustration':
+      case "chosen_illustration":
         return BookCoverMode.chosenIllustration;
-      case 'custom_cover':
+      case "custom_cover":
         return BookCoverMode.customCover;
       default:
         return BookCoverMode.lastIllustrationAdded;
@@ -61,21 +63,21 @@ class BookCover {
 
   Map<String, dynamic> toMap() {
     return {
-      'mode': modeToString(),
-      'link': link,
+      "mode": modeToString(),
+      "link": link,
     };
   }
 
   String modeToString() {
     switch (mode) {
       case BookCoverMode.chosenIllustration:
-        return 'chosen_illustration';
+        return "chosen_illustration";
       case BookCoverMode.customCover:
-        return 'custom_cover';
+        return "custom_cover";
       case BookCoverMode.lastIllustrationAdded:
-        return 'last_illustration_added';
+        return "last_illustration_added";
       default:
-        return 'last_illustration_added';
+        return "last_illustration_added";
     }
   }
 
@@ -86,7 +88,7 @@ class BookCover {
 
   @override
   String toString() =>
-      'BookCover(mode: $mode, link: $link, updatedAt: $updatedAt)';
+      "BookCover(mode: $mode, link: $link, updatedAt: $updatedAt)";
 
   @override
   bool operator ==(Object other) {
