2ed386e6a076fb42583e7c1336856f601788a75a
rootasjey <jeremie@rootasjey.dev>
2022-04-08 16:43:11 +0200

    feat: :sparkles: add `EnumSectionVisibility`
    Choose if a section should only be visible to staff members or available to all users.

---
+++ lib/types/enums/enum_section_visibility.dart
@@ -0,0 +1,8 @@
+/// Section's visibility.
+enum EnumSectionVisibility {
+  /// Everybody can use this section (i.e. use it on a modular/profile page).
+  public,
+
+  /// Only staff members can use this section (i.e. use it on a modular page).
+  staff,
+}
