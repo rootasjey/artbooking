60f8dc4187d55a6864cded27a726c39eb736733f
rootasjey <jeremie@rootasjey.dev>
2022-04-22 13:54:03 +0200

    feat: :sparkles: update `BetterAvatar` to support dnd
    Add drag and drop support to `BetterAvatar`.

--- lib/components/avatar/better_avatar.dart
+++ lib/components/avatar/better_avatar.dart
@@ -1,22 +1,60 @@
+import 'package:artbooking/globals/constants.dart';
+import 'package:artbooking/globals/utilities.dart';
+import 'package:artbooking/types/drag_data.dart';
+import 'package:artbooking/types/enums/enum_artist_item_action.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:supercharged/supercharged.dart';
+import 'package:unicons/unicons.dart';
 
 class BetterAvatar extends StatefulWidget {
-  final VoidCallback? onTap;
-  final ImageProvider<Object> image;
-  final double size;
-  final double elevation;
-
-  /// Not used if onTap is null.
-  final ColorFilter? colorFilter;
-
   BetterAvatar({
     required this.image,
     this.elevation = 4.0,
     this.onTap,
     this.size = 220.0,
     this.colorFilter,
+    this.useAsPlaceholder = false,
+    this.index = -1,
+    this.canDrag = false,
+    this.popupMenuEntries = const [],
+    this.onDrop,
+    this.onPopupMenuItemSelected,
+    this.id = "",
+    this.title = "",
+    this.padding = EdgeInsets.zero,
+    this.dragGroupName = "",
   });
+
+  final bool useAsPlaceholder;
+
+  /// Not used if onTap is null.
+  final ColorFilter? colorFilter;
+  final double size;
+  final double elevation;
+  final VoidCallback? onTap;
+  final ImageProvider<Object> image;
+  final EdgeInsets padding;
+
+  final int index;
+  final bool canDrag;
+
+  /// Popup menu item entries.
+  final List<PopupMenuEntry<EnumArtistItemAction>> popupMenuEntries;
+
+  /// Callback function when popup menu item entries are tapped.
+  final void Function(
+    EnumArtistItemAction itemAction,
+    int index,
+    String artistId,
+  )? onPopupMenuItemSelected;
+
+  /// Callback when drag and dropping items on this illustration card.
+  final void Function(int dropTargetIndex, List<int> dragIndexes)? onDrop;
+
+  final String id;
+  final String title;
+  final String dragGroupName;
 
   @override
   _BetterAvatarState createState() => _BetterAvatarState();
@@ -36,13 +74,13 @@ class _BetterAvatarState extends State<BetterAvatar>
     scaleAnimationController = AnimationController(
       lowerBound: 0.8,
       upperBound: 1.0,
-      duration: 500.milliseconds,
+      duration: 250.milliseconds,
       vsync: this,
     );
 
     scaleAnimation = CurvedAnimation(
       parent: scaleAnimationController,
-      curve: Curves.fastOutSlowIn,
+      curve: Curves.elasticOut,
     );
 
     setState(() {
@@ -58,18 +96,181 @@ class _BetterAvatarState extends State<BetterAvatar>
 
   @override
   Widget build(BuildContext context) {
-    if (widget.onTap == null) {
-      return withoutLink();
+    if (widget.useAsPlaceholder) {
+      return placeholder();
     }
 
-    return withLink();
+    if (!widget.canDrag) {
+      return avatarWidget();
+    }
+
+    return DragTarget<DragData>(
+      builder: (context, candidateItems, rejectedItems) {
+        return avatarWidget(
+          usingAsDroptarget: candidateItems.isNotEmpty,
+        );
+      },
+      onAccept: (DragData dragData) {
+        widget.onDrop?.call(widget.index, [dragData.index]);
+      },
+      onWillAccept: (DragData? dragData) {
+        if (dragData == null) {
+          return false;
+        }
+
+        if (dragData.type != BetterAvatar) {
+          return false;
+        }
+
+        if (dragData.groupName != widget.dragGroupName) {
+          return false;
+        }
+
+        return true;
+      },
+    );
   }
 
-  Widget withLink() {
+  Widget avatarWidget({bool usingAsDroptarget = false}) {
+    return Padding(
+      padding: widget.padding,
+      child: Column(
+        children: [
+          imageContainerWidget(
+            usingAsDroptarget: usingAsDroptarget,
+          ),
+          if (widget.title.isNotEmpty)
+            Text(
+              widget.title,
+              style: Utilities.fonts.style(
+                fontSize: 16.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          if (widget.popupMenuEntries.isNotEmpty) popupMenuButton()
+        ],
+      ),
+    );
+  }
+
+  Widget imageContainerWidget({bool usingAsDroptarget = false}) {
+    Widget avatarImage = ScaleTransition(
+      scale: scaleAnimation,
+      child: imageWidget(
+        usingAsDroptarget: usingAsDroptarget,
+      ),
+    );
+
+    if (widget.canDrag) {
+      avatarImage = LongPressDraggable<DragData>(
+        data: DragData(
+          groupName: widget.dragGroupName,
+          index: widget.index,
+          type: BetterAvatar,
+        ),
+        child: avatarImage,
+        childWhenDragging: childWhenDragging(),
+        feedback: imageWidget(
+          dragging: true,
+        ),
+      );
+    }
+
+    return avatarImage;
+  }
+
+  Widget imageWidget({
+    bool usingAsDroptarget = false,
+    bool dragging = false,
+  }) {
+    final double size = dragging ? widget.size / 2 : widget.size;
+
+    final Color borderColor = usingAsDroptarget
+        ? Constants.colors.tertiary
+        : Theme.of(context).primaryColor;
+
+    return Material(
+      elevation: elevation,
+      color: Theme.of(context).backgroundColor,
+      clipBehavior: Clip.antiAlias,
+      shape: CircleBorder(
+        side: BorderSide(
+          color: borderColor,
+          width: 3.0,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Material(
+          clipBehavior: Clip.antiAlias,
+          shape: CircleBorder(),
+          child: SizedBox(
+            width: size,
+            height: size,
+            child: Ink.image(
+              image: widget.image,
+              width: size,
+              height: size,
+              fit: BoxFit.cover,
+              colorFilter: widget.colorFilter,
+              child: InkWell(
+                onTap: widget.onTap,
+                onHover: (isHover) {
+                  if (isHover) {
+                    elevation = (widget.elevation + 1.0) * 2;
+                    scaleAnimationController.forward();
+                    setState(() {});
+                    return;
+                  }
+
+                  elevation = widget.elevation;
+                  scaleAnimationController.reverse();
+                  setState(() {});
+                },
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget childWhenDragging() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Material(
+        color: Theme.of(context).backgroundColor,
+        clipBehavior: Clip.antiAlias,
+        shape: CircleBorder(
+          side: BorderSide(
+            color: Theme.of(context).primaryColor,
+            width: 3.0,
+          ),
+        ),
+        child: Container(
+          width: widget.size,
+          height: widget.size,
+          padding: const EdgeInsets.all(8.0),
+          child: DottedBorder(
+            strokeWidth: 3.0,
+            borderType: BorderType.Circle,
+            radius: Radius.circular(16),
+            color: Theme.of(context).primaryColor.withOpacity(0.6),
+            dashPattern: [8, 4],
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(16)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget placeholder() {
     final size = widget.size;
 
-    return ScaleTransition(
-      scale: scaleAnimation,
+    return Padding(
+      padding: widget.padding,
       child: Material(
         elevation: elevation,
         color: Theme.of(context).backgroundColor,
@@ -85,30 +286,12 @@ class _BetterAvatarState extends State<BetterAvatar>
           child: Material(
             clipBehavior: Clip.antiAlias,
             shape: CircleBorder(),
-            child: SizedBox(
-              width: size,
-              height: size,
-              child: Ink.image(
-                image: widget.image,
+            child: InkWell(
+              onTap: widget.onTap,
+              child: SizedBox(
                 width: size,
                 height: size,
-                fit: BoxFit.cover,
-                colorFilter: widget.colorFilter,
-                child: InkWell(
-                  onTap: widget.onTap,
-                  onHover: (isHover) {
-                    if (isHover) {
-                      elevation = (widget.elevation + 1.0) * 2;
-                      scaleAnimationController.forward();
-                      setState(() {});
-                      return;
-                    }
-
-                    elevation = widget.elevation;
-                    scaleAnimationController.reverse();
-                    setState(() {});
-                  },
-                ),
+                child: Icon(UniconsLine.plus),
               ),
             ),
           ),
@@ -117,36 +300,20 @@ class _BetterAvatarState extends State<BetterAvatar>
     );
   }
 
-  Widget withoutLink() {
-    final size = widget.size;
-
-    return Material(
-      elevation: elevation,
-      color: Theme.of(context).backgroundColor,
-      clipBehavior: Clip.antiAlias,
-      shape: CircleBorder(
-        side: BorderSide(
-          color: Theme.of(context).primaryColor,
-          width: 3.0,
-        ),
+  Widget popupMenuButton() {
+    return PopupMenuButton<EnumArtistItemAction>(
+      icon: Icon(
+        UniconsLine.ellipsis_h,
+        color: Theme.of(context).secondaryHeaderColor,
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Material(
-          clipBehavior: Clip.antiAlias,
-          shape: CircleBorder(),
-          child: SizedBox(
-            width: size,
-            height: size,
-            child: Image(
-              image: widget.image,
-              width: size,
-              height: size,
-              fit: BoxFit.cover,
-            ),
-          ),
-        ),
-      ),
+      onSelected: (EnumArtistItemAction action) {
+        widget.onPopupMenuItemSelected?.call(
+          action,
+          widget.index,
+          widget.id,
+        );
+      },
+      itemBuilder: (_) => widget.popupMenuEntries,
     );
   }
 }
