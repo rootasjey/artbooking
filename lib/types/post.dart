1c70d01c6f99801b7d170e149215dba7e9583591
rootasjey <jeremie@rootasjey.dev>
2022-05-10 00:37:34 +0200

    feat: :label: add `publishedAt`, `content` properties
    Add `tagsToMap()` method to `Post` class.

--- lib/types/post.dart
+++ lib/types/post.dart
@@ -8,6 +8,7 @@ import 'package:artbooking/types/enums/enum_content_visibility.dart';
 class Post {
   Post({
     required this.createdAt,
+    required this.publishedAt,
     required this.updatedAt,
     required this.visibility,
     this.authors = const [],
@@ -20,6 +21,7 @@ class Post {
     this.translations = const [],
     this.wordCount = 0,
     this.iconData = "",
+    this.content = "",
   });
 
   final List<String> authors;
@@ -29,16 +31,19 @@ class Post {
   final String name;
   final String storagePath;
   final String id;
+  final DateTime publishedAt;
   final List<String> tags;
   final List<String> translations;
   final EnumContentVisibility visibility;
   final DateTime updatedAt;
   final int wordCount;
   final String iconData;
+  final String content;
 
   factory Post.empty() {
     return Post(
       createdAt: DateTime.now(),
+      publishedAt: DateTime.now(),
       visibility: EnumContentVisibility.private,
       updatedAt: DateTime.now(),
     );
@@ -53,6 +58,8 @@ class Post {
     String? storagePath,
     String? id,
     String? iconData,
+    String? content,
+    DateTime? publishedAt,
     List<String>? tags,
     List<String>? translations,
     EnumContentVisibility? visibility,
@@ -64,10 +71,12 @@ class Post {
       createdAt: createdAt ?? this.createdAt,
       description: description ?? this.description,
       iconData: iconData ?? this.iconData,
+      content: content ?? this.content,
       languages: languages ?? this.languages,
       name: name ?? this.name,
       storagePath: storagePath ?? this.storagePath,
       id: id ?? this.id,
+      publishedAt: publishedAt ?? this.publishedAt,
       tags: tags ?? this.tags,
       translations: translations ?? this.translations,
       visibility: visibility ?? this.visibility,
@@ -79,6 +88,7 @@ class Post {
   Map<String, dynamic> toMap() {
     return {
       'authors': authors,
+      'content': content,
       'created_at': createdAt.millisecondsSinceEpoch,
       'description': description,
       'icon_data': iconData,
@@ -86,6 +96,7 @@ class Post {
       'name': name,
       'storagePath': storagePath,
       'id': id,
+      'published_at': createdAt.millisecondsSinceEpoch,
       'tags': tags,
       'translations': translations,
       'visibility': visibility.name,
@@ -94,9 +105,24 @@ class Post {
     };
   }
 
-  factory Post.fromMap(Map<String, dynamic> map) {
+  Map<String, bool> tagsToMap() {
+    final map = <String, bool>{};
+
+    for (final String tag in tags) {
+      map.putIfAbsent(tag, () => true);
+    }
+
+    return map;
+  }
+
+  factory Post.fromMap(Map<String, dynamic>? map) {
+    if (map == null) {
+      return Post.empty();
+    }
+
     return Post(
       authors: parseMapToArray(map["authors"]),
+      content: map["content"] ?? "",
       createdAt: Utilities.date.fromFirestore(map["created_at"]),
       description: map["description"] ?? "",
       iconData: map["icon_data"] ?? "",
@@ -104,6 +130,7 @@ class Post {
       name: map["name"] ?? "",
       storagePath: map["storage_path"] ?? "",
       id: map["id"] ?? "",
+      publishedAt: Utilities.date.fromFirestore(map["published_at"]),
       tags: parseMapToArray(map["tags"]),
       translations: parseMapToArray(map["translations"]),
       visibility: parseVisibility(map["visibility"]),
@@ -151,11 +178,11 @@ class Post {
 
   @override
   String toString() {
-    return "Post(authors: $authors, createdAt: $createdAt, "
+    return "Post(authors: $authors, content: $content, createdAt: $createdAt, "
         "description: $description, iconData: $iconData, languages: $languages, "
-        "name: $name, storagePath: $storagePath, id: $id, tags: $tags, "
-        "translations: $translations, visibility: $visibility, "
-        "updatedAt: $updatedAt, wordCount: $wordCount)";
+        "name: $name, pubslihedAt: $publishedAt, id: $id, tags: $tags, "
+        "storagePath: $storagePath, translations: $translations, "
+        "updatedAt: $updatedAt, visibility: $visibility, wordCount: $wordCount)";
   }
 
   @override
@@ -163,6 +190,7 @@ class Post {
     if (identical(this, other)) return true;
 
     return other is Post &&
+        content == content &&
         listEquals(other.authors, authors) &&
         other.createdAt == createdAt &&
         other.description == description &&
@@ -170,6 +198,7 @@ class Post {
         other.name == name &&
         other.storagePath == storagePath &&
         other.id == id &&
+        other.publishedAt == publishedAt &&
         listEquals(other.tags, tags) &&
         listEquals(other.translations, translations) &&
         other.visibility == visibility &&
@@ -180,12 +209,14 @@ class Post {
   @override
   int get hashCode {
     return authors.hashCode ^
+        content.hashCode ^
         createdAt.hashCode ^
         description.hashCode ^
         languages.hashCode ^
         name.hashCode ^
         storagePath.hashCode ^
         id.hashCode ^
+        publishedAt.hashCode ^
         tags.hashCode ^
         translations.hashCode ^
         visibility.hashCode ^
