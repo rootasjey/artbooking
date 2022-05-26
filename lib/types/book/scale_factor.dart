5060b238db016097ae8dd8201e6b7f025668bf97
rootasjey <jeremie@rootasjey.dev>
2022-05-27 01:46:14 +0200

    feat: :fire: remove mobile props in `ScaleFactor`
    These properties are not used at the moment.

--- lib/types/book/scale_factor.dart
+++ lib/types/book/scale_factor.dart
@@ -2,22 +2,20 @@ import 'dart:convert';
 
 class ScaleFactor {
   const ScaleFactor({
-    this.height = 0,
-    this.mobileHeight = 0,
-    this.mobileWidth = 0,
-    this.width = 0,
+    this.height = 1,
+    this.width = 1,
   });
 
   final int height;
-  final int mobileHeight;
-  final int mobileWidth;
   final int width;
 
-  factory ScaleFactor.fromMap(Map<String, dynamic> data) {
+  factory ScaleFactor.fromMap(Map<String, dynamic>? data) {
+    if (data == null) {
+      return ScaleFactor();
+    }
+
     return ScaleFactor(
       height: data['height']?.toInt() ?? 1,
-      mobileHeight: data['mobile_height']?.toInt() ?? 1,
-      mobileWidth: data['mobile_width']?.toInt() ?? 1,
       width: data['width']?.toInt() ?? 1,
     );
   }
@@ -25,22 +23,16 @@ class ScaleFactor {
   Map<String, dynamic> toMap() {
     return {
       'height': height,
-      'mobile_height': mobileHeight,
-      'mobile_width': mobileWidth,
       'width': width,
     };
   }
 
   ScaleFactor copyWith({
     int? height,
-    int? mobileHeight,
-    int? mobileWidth,
     int? width,
   }) {
     return ScaleFactor(
       height: height ?? this.height,
-      mobileHeight: mobileHeight ?? this.mobileHeight,
-      mobileWidth: mobileWidth ?? this.mobileWidth,
       width: width ?? this.width,
     );
   }
@@ -52,7 +44,7 @@ class ScaleFactor {
 
   @override
   String toString() {
-    return 'ScaleFactor(height: $height, mobileHeight: $mobileHeight, mobileWidth: $mobileWidth, width: $width)';
+    return 'ScaleFactor(height: $height, width: $width)';
   }
 
   @override
@@ -61,16 +53,11 @@ class ScaleFactor {
 
     return other is ScaleFactor &&
         other.height == height &&
-        other.mobileHeight == mobileHeight &&
-        other.mobileWidth == mobileWidth &&
         other.width == width;
   }
 
   @override
   int get hashCode {
-    return height.hashCode ^
-        mobileHeight.hashCode ^
-        mobileWidth.hashCode ^
-        width.hashCode;
+    return height.hashCode ^ width.hashCode;
   }
 }
