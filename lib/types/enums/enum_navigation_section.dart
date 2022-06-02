844a17837aa98a617c0d27456463e9fbaece13c1
rootasjey <jeremie@rootasjey.dev>
2022-06-02 22:31:31 +0200

    feat: :label: add `EnumNavigationSection`
    Case scenarios for navigation from section.

---
+++ lib/types/enums/enum_navigation_section.dart
@@ -0,0 +1,5 @@
+/// Case-scenario for section navigation (e.g. "see more" button)
+enum EnumNavigationSection {
+  /// Navigate to app all illustration or an user's personal illustrations.
+  illustrations,
+}
