31b974d7a0ed8ab11bd5b592d100bab1e73f8c07
rootasjey <jeremie@rootasjey.dev>
2022-06-02 22:32:25 +0200

    feat: :label: add `EnumPageType.home` value
    Add this missing value ("home") in enum.

--- lib/types/enums/enum_page_type.dart
+++ lib/types/enums/enum_page_type.dart
@@ -1,5 +1,8 @@
 /// User's page types.
 enum EnumPageType {
+  /// Home page created by staff.
+  home,
+
   /// A page to show an user's profile.
   /// This page can contain user's profile picture, they last llustrations,
   /// books, in any order.
