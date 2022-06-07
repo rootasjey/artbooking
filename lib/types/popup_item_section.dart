8c6ff8a8c28d6818713ef90c7f540f4e0cef7ef1
rootasjey <jeremie@rootasjey.dev>
2022-06-07 05:09:21 +0200

    feat: :label: add `PopupMenuItemSection` typedef alias
    `PopupMenuItemSection` `=` `PopupMenuItemIcon<EnumSectionAction>`.

---
+++ lib/types/popup_item_section.dart
@@ -0,0 +1,4 @@
+import 'package:artbooking/components/popup_menu/popup_menu_item_icon.dart';
+import 'package:artbooking/types/enums/enum_section_action.dart';
+
+typedef PopupMenuItemSection = PopupMenuItemIcon<EnumSectionAction>;
