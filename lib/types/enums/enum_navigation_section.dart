ecb15ba5b941616469c94199a842ed29a9f428e7
rootasjey <jeremie@rootasjey.dev>
2022-06-12 10:15:48 +0200

    feat: :label: add `EnumNavigationSection.books` value

--- lib/types/enums/enum_navigation_section.dart
+++ lib/types/enums/enum_navigation_section.dart
@@ -1,5 +1,8 @@
 /// Case-scenario for section navigation (e.g. "see more" button)
 enum EnumNavigationSection {
+  /// Navigate to app all books or an user's personal bookss.
+  books,
+
   /// Navigate to app all illustration or an user's personal illustrations.
   illustrations,
 }
