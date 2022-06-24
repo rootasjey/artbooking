0abe352c05d218dca1105f9d1453d086c1eeb882
rootasjey <jeremie@rootasjey.dev>
2022-06-25 01:35:55 +0200

    feat: :label: add `EnumShareContentType`

---
+++ lib/types/enums/enum_share_content_type.dart
@@ -0,0 +1,8 @@
+/// Content type to share.
+enum EnumShareContentType {
+  /// The content being shared is a book (as a link or it's image cover).
+  book,
+
+  /// The content being shared is an illustration (as a link or it's thumbnail).
+  illustration,
+}
