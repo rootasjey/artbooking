84c7b2bc6c2e8d76f42b3ee34043bccfed4862a8
rootasjey <jeremie@rootasjey.dev>
2022-07-09 01:03:14 +0200

    feat: :sparkles: add `selectedColor` & `copyWith(..)` to `PopupMenuItemIcon`
    The icon is now in the same color of the text if it's a `PopupMenuIcon` and the item is `selected`.

--- lib/components/popup_menu/popup_menu_item_icon.dart
+++ lib/components/popup_menu/popup_menu_item_icon.dart
@@ -1,4 +1,5 @@
 import 'package:artbooking/components/animations/fade_in_y.dart';
+import 'package:artbooking/components/popup_menu/popup_menu_icon.dart';
 import 'package:artbooking/globals/utilities.dart';
 import 'package:flutter/material.dart';
 import 'package:unicons/unicons.dart';
@@ -16,6 +17,7 @@ class PopupMenuItemIcon<T> extends PopupMenuItem<T> {
     this.value,
     this.selected = false,
     this.delay = const Duration(seconds: 0),
+    this.selectedColor,
   }) : super(
           key: key,
           value: value,
@@ -35,18 +37,28 @@ class PopupMenuItemIcon<T> extends PopupMenuItem<T> {
                   children: [
                     Padding(
                       padding: const EdgeInsets.only(right: 18.0),
-                      child: icon,
+                      child: icon.runtimeType == PopupMenuIcon
+                          ? PopupMenuIcon(
+                              (icon as PopupMenuIcon).iconData,
+                              color: selectedColor,
+                            )
+                          : icon,
                     ),
                     Expanded(
                       child: Text(
                         textLabel,
                         style: Utilities.fonts.body(
+                          color: selectedColor,
                           fontSize: 14.0,
                           fontWeight: FontWeight.w700,
                         ),
                       ),
                     ),
-                    if (selected) Icon(UniconsLine.check),
+                    if (selected)
+                      Icon(
+                        UniconsLine.check,
+                        color: selectedColor,
+                      ),
                   ],
                 ),
               ),
@@ -58,9 +70,36 @@ class PopupMenuItemIcon<T> extends PopupMenuItem<T> {
   final String textLabel;
   final T? value;
   final bool enabled;
+  final Color? selectedColor;
   final double height;
   final EdgeInsets? padding;
   final MouseCursor? mouseCursor;
   final bool selected;
   final Duration delay;
+
+  PopupMenuItemIcon<T> copyWith({
+    bool? enabled,
+    bool? selected,
+    Color? selectedColor,
+    double? height,
+    Duration? delay,
+    EdgeInsets? padding,
+    MouseCursor? mouseCursor,
+    String? textLabel,
+    T? value,
+    Widget? icon,
+  }) {
+    return PopupMenuItemIcon(
+      enabled: enabled ?? this.enabled,
+      selected: selected ?? this.selected,
+      selectedColor: selectedColor ?? this.selectedColor,
+      height: height ?? this.height,
+      delay: delay ?? this.delay,
+      padding: padding ?? this.padding,
+      mouseCursor: mouseCursor ?? this.mouseCursor,
+      textLabel: textLabel ?? this.textLabel,
+      value: value ?? this.value,
+      icon: icon ?? this.icon,
+    );
+  }
 }
