7f6a5f125f9724b6a697e3ae89ed2992a03e5ffc
rootasjey <jeremie@rootasjey.dev>
2022-07-17 01:27:37 +0200

    feat: :label: add `EnumSearchItemType`

---
+++ lib/types/enums/enum_search_item_type.dart
@@ -0,0 +1,11 @@
+/// Useful to know which type is a search result item.
+enum EnumSearchItemType {
+  /// This search result is a book.
+  book,
+
+  /// This search result is an illustration.
+  illustration,
+
+  /// This search result is an user.
+  user,
+}
