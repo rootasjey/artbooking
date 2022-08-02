a704fff74c97c7fa05ef31b56abf0e0fcb4559c8
rootasjey <jeremie@rootasjey.dev>
2022-08-03 01:40:25 +0200

    feat: :sparkles: add `margin` prop. to `DarkOutlinedButton`

--- lib/components/buttons/dark_outlined_button.dart
+++ lib/components/buttons/dark_outlined_button.dart
@@ -4,15 +4,25 @@ import 'package:flutter/material.dart';
 class DarkOutlinedButton extends StatelessWidget {
   const DarkOutlinedButton({
     Key? key,
-    this.onPressed,
     required this.child,
-    this.selected = false,
     this.accentColor,
+    this.margin = EdgeInsets.zero,
+    this.onPressed,
+    this.selected = false,
   }) : super(key: key);
 
+  /// This button will be highlited with `accentColor` if this is true.
   final bool selected;
+
+  /// Button's main color (borders, text when selected).
   final Color? accentColor;
-  final Function()? onPressed;
+
+  final EdgeInsets margin;
+
+  /// Callback fired when this button is pressed.
+  final void Function()? onPressed;
+
+  /// Child widget of this button.
   final Widget child;
 
   @override
@@ -24,28 +34,31 @@ class DarkOutlinedButton extends StatelessWidget {
         Theme.of(context).textTheme.bodyText2?.color?.withOpacity(0.6) ??
             Colors.black;
 
-    return OutlinedButton(
-      onPressed: onPressed,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 8.0,
-          vertical: 6.0,
+    return Padding(
+      padding: margin,
+      child: OutlinedButton(
+        onPressed: onPressed,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 8.0,
+            vertical: 6.0,
+          ),
+          child: child,
         ),
-        child: child,
-      ),
-      style: OutlinedButton.styleFrom(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(2.0),
-        ),
-        side: selected
-            ? BorderSide(
-                color: _accentColor,
-                width: 2.0,
-              )
-            : null,
-        primary: selected ? _accentColor : baseColor.withOpacity(0.4),
-        textStyle: Utilities.fonts.body(
-          fontWeight: FontWeight.w600,
+        style: OutlinedButton.styleFrom(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(2.0),
+          ),
+          side: selected
+              ? BorderSide(
+                  color: _accentColor,
+                  width: 2.0,
+                )
+              : null,
+          primary: selected ? _accentColor : baseColor.withOpacity(0.4),
+          textStyle: Utilities.fonts.body(
+            fontWeight: FontWeight.w600,
+          ),
         ),
       ),
     );
