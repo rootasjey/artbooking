3267cb6414de200f1e1471fcbdd09d940d3ea4f3
rootasjey <jeremie@rootasjey.dev>
2022-08-24 23:58:42 +0200

    feat: :sparkles: add `DarkTextButton.margin` property

--- lib/components/buttons/dark_text_button.dart
+++ lib/components/buttons/dark_text_button.dart
@@ -5,27 +5,32 @@ class DarkTextButton extends StatelessWidget {
   const DarkTextButton({
     Key? key,
     required this.child,
-    this.onPressed,
     this.backgroundColor,
+    this.margin = EdgeInsets.zero,
+    this.onPressed,
   }) : super(key: key);
 
+  final Color? backgroundColor;
+  final EdgeInsets margin;
   final void Function()? onPressed;
   final Widget child;
-  final Color? backgroundColor;
 
   @override
   Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: onPressed,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: child,
-      ),
-      style: TextButton.styleFrom(
-        primary: Theme.of(context).textTheme.bodyText1?.color,
-        backgroundColor: backgroundColor,
-        textStyle: Utilities.fonts.body(
-          fontWeight: FontWeight.w600,
+    return Padding(
+      padding: margin,
+      child: TextButton(
+        onPressed: onPressed,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: child,
+        ),
+        style: TextButton.styleFrom(
+          primary: Theme.of(context).textTheme.bodyText1?.color,
+          backgroundColor: backgroundColor,
+          textStyle: Utilities.fonts.body(
+            fontWeight: FontWeight.w600,
+          ),
         ),
       ),
     );
@@ -35,23 +40,27 @@ class DarkTextButton extends StatelessWidget {
     void Function()? onPressed,
     required Widget child,
     Color? backgroundColor,
+    EdgeInsets margin = EdgeInsets.zero,
   }) {
-    return TextButton(
-      style: TextButton.styleFrom(
-        primary: Colors.black87,
-        minimumSize: Size(200.0, 0.0),
-        backgroundColor: backgroundColor,
-        textStyle: Utilities.fonts.body(
-          fontWeight: FontWeight.w500,
+    return Padding(
+      padding: margin,
+      child: TextButton(
+        style: TextButton.styleFrom(
+          primary: Colors.black87,
+          minimumSize: Size(200.0, 0.0),
+          backgroundColor: backgroundColor,
+          textStyle: Utilities.fonts.body(
+            fontWeight: FontWeight.w500,
+          ),
         ),
-      ),
-      onPressed: onPressed,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16.0,
-          vertical: 12.0,
+        onPressed: onPressed,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 12.0,
+          ),
+          child: child,
         ),
-        child: child,
       ),
     );
   }
